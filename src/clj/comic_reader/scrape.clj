719e76bdf1eb25f035c3293e2045a03b5b1a5ce3
Geoff Shannon <geoffpshannon@gmail.com>
2015-03-30 16:10:06 -0700

    Start working on comic-list processing

--- src/clj/comic_reader/scrape.clj
+++ src/clj/comic_reader/scrape.clj
@@ -1,2 +1,22 @@
 (ns comic-reader.scrape
-  (:require [net.cgrand.enlive-html :as html]))
+  (:require [clojure.string :refer [trim]]
+            [net.cgrand.enlive-html :as html]))
+
+(def sites
+  {"Manga Fox" {:url "http://mangafox.me/manga/"
+                :selector [:div.manga_list :ul :li :a]
+                :normalize (fn [{name :content
+                                 {url :href} :attrs}]
+                             {:name name
+                              :url url})}
+   "Manga Reader" {:url "http://www.mangareader.net/alphabetical"
+                   :selector [:div.series_alpha :ul :li :a]
+                   :normalize (fn [{name :content
+                                    {url :href} :attrs}]
+                                {:name (trim name)
+                                 :url url})}})
+
+(defn fetch-url [url]
+  (html/html-resource (java.net.URL. url)))
+
+(defn fetch-site-list [{:keys [url selector normalize]}])
