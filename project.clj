617398866c77276297beae34b22c0f79e4262452
Geoff Shannon <geoffpshannon@gmail.com>
2015-03-30 13:14:14 -0700

    Move clojure source into sub dir of src

--- project.clj
+++ project.clj
@@ -3,6 +3,9 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+
+  :source-paths ["src/clj"]
+
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/clojurescript "0.0-2371"]
                  [ring "1.3.1"]
