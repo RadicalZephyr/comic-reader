eb81165a8840469d97250df68fb63939439922c0
Geoff Shannon <geoffpshannon@gmail.com>
2015-03-30 16:20:32 -0700

    Restructure site meta-data

--- src/clj/comic_reader/scrape.clj
+++ src/clj/comic_reader/scrape.clj
@@ -4,18 +4,20 @@
   (:import java.net.URL))
 
 (def sites
-  {"Manga Fox" {:url "http://mangafox.me/manga/"
-                :selector [:div.manga_list :ul :li :a]
-                :normalize (fn [{name :content
-                                 {url :href} :attrs}]
-                             {:name name
-                              :url url})}
-   "Manga Reader" {:url "http://www.mangareader.net/alphabetical"
-                   :selector [:div.series_alpha :ul :li :a]
-                   :normalize (fn [{name :content
-                                    {url :href} :attrs}]
-                                {:name (trim name)
-                                 :url url})}})
+  [{:name "Manga Fox"
+    :url "http://mangafox.me/manga/"
+    :selector [:div.manga_list :ul :li :a]
+    :normalize (fn [{[name] :content
+                     {url :href} :attrs}]
+                 {:name name
+                  :url url})}
+   {:name "Manga Reader"
+    :url "http://www.mangareader.net/alphabetical"
+    :selector [:div.series_alpha :ul :li :a]
+    :normalize (fn [{[name] :content
+                     {url :href} :attrs}]
+                 {:name (trim name)
+                  :url url})}])
 
 (defn fetch-url [url]
   (html/html-resource (java.net.URL. url)))
