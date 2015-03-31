a7349e62ed6fce3f7ccd07860b300b8dcb64c8a1
Geoff Shannon <geoffpshannon@gmail.com>
2015-03-30 18:08:54 -0700

    Add figwheel client setups

---
+++ src/cljs/comic_reader/main.cljs
@@ -0,0 +1,26 @@
+(ns cljs.comic-reader.main
+  (:require [figwheel.client :as fw]))
+
+(enable-console-print!)
+
+(fw/start {
+           ;; configure a websocket url if you are using your own server
+           ;; :websocket-url "ws://localhost:3449/figwheel-ws"
+
+           ;; optional callback
+           :on-jsload (fn [] (print "reloaded"))
+
+           ;; The heads up display is enabled by default
+           ;; to disable it:
+           ;; :heads-up-display false
+
+           ;; when the compiler emits warnings figwheel
+           ;; blocks the loading of files.
+           ;; To disable this behavior:
+           ;; :load-warninged-code true
+
+           ;; if figwheel is watching more than one build
+           ;; it can be helpful to specify a build id for
+           ;; the client to focus on
+           ;; :build-id "example"
+           })
