1f9a65b990c5814ea048861d53762f4e1d8fe053
Zefira Shannon <earthlingzephyr@gmail.com>
2015-03-30 17:20:52 -0700

    Add a url->feed function to manga fox data

--- src/clj/comic_reader/scrape.clj
+++ src/clj/comic_reader/scrape.clj
@@ -1,23 +1,31 @@
 (ns comic-reader.scrape
-  (:require [clojure.string :refer [trim]]
+  (:require [clojure.string :as s]
             [net.cgrand.enlive-html :as html])
   (:import java.net.URL))
 
 (def sites
-  [{:name "Manga Fox"
-    :url "http://mangafox.me/manga/"
-    :selector [:div.manga_list :ul :li :a]
-    :normalize (fn [{[name] :content
-                     {url :href} :attrs}]
-                 {:name name
-                  :url url})}
+  [(let [canonical-url "http://mangafox.me"
+         manga-url (format "%s/manga/" canonical-url)
+         manga-pattern (re-pattern (str manga-url "(.*?)/"))]
+     {:name "Manga Fox"
+      :url manga-url
+      :selector [:div.manga_list :ul :li :a]
+      :normalize (fn [{[name] :content
+                       {url :href} :attrs}]
+                   {:name name
+                    :url url})
+      :url->feed (fn [url]
+                   (some->> url
+                            (re-matches manga-pattern)
+                            second
+                            (format "%s/rss/%s.xml" canonical-url)))})
    (let [canonical-url "http://www.mangareader.net/"]
      {:name "Manga Reader"
       :url (str canonical-url "alphabetical")
       :selector [:div.series_alpha :ul :li :a]
       :normalize (fn [{[name] :content
                        {url :href} :attrs}]
-                   {:name (trim name)
+                   {:name (s/trim name)
                     :url (str canonical-url url)})})])
 
 (defn fetch-feed [url]
