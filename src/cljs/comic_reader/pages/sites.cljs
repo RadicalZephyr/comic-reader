f1930511d11b6c8b3abc0eb07bcbfbf080dbd464
Geoff Shannon <geoffpshannon@gmail.com>
2015-03-31 13:42:56 -0700

    Move site components into site ns

--- src/cljs/comic_reader/pages/sites.cljs
+++ src/cljs/comic_reader/pages/sites.cljs
@@ -1,1 +1,10 @@
 (ns comic-reader.pages.sites)
+
+(defn manga-site [site-data]
+  [:li (:name site-data)])
+
+(defn site-list [data]
+  (let [data @data]
+    [:div
+     [:h1 (:heading data)]
+     [:ul (map manga-site (:sites data))]]))
