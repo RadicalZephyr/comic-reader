eb35ed1f78c7173963a033eaa0574e64e0d74b24
Geoff Shannon <geoffpshannon@gmail.com>
2015-03-31 13:34:13 -0700

    Add secretary and remove sablono

--- project.clj
+++ project.clj
@@ -14,7 +14,7 @@
                  [org.clojure/clojurescript "0.0-2850"]
                  [figwheel "0.2.5-SNAPSHOT"]
                  [reagent "0.5.0"]
-                 [sablono "0.3.4"]
+                 [secretary "1.2.3"]
                  [cljs-ajax "0.3.10"]]
 
   :plugins      [[lein-ring "0.8.13"]
