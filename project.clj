26814a8ceeb7ea9df2b7c7c75c8ccc4a66b27f4d
Geoff Shannon <geoffpshannon@gmail.com>
2015-03-30 17:57:05 -0700

    Reconfigure project for usage of figwheel

--- project.clj
+++ project.clj
@@ -7,23 +7,26 @@
   :source-paths ["src/clj"]
 
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/clojurescript "0.0-2371"]
+                 [org.clojure/clojurescript "0.0-2843"]
+                 [figwheel "0.2.5"]
                  [enlive "1.1.5"]
                  [ring "1.3.1"]
                  [compojure "1.2.1"]
                  [cljs-ajax "0.3.3"]]
 
   :plugins      [[lein-ring "0.8.13"]
-                 [lein-cljsbuild "1.0.3"]]
+                 [lein-cljsbuild "1.0.4"]
+                 [lein-figwheel "0.2.5"]]
 
   :hooks [leiningen.cljsbuild]
   :ring  {:handler comic-reader.core/app}
   :cljsbuild {:builds [{:source-paths ["src/cljs"]
-                        :compiler {:output-dir "resources/public/js"
-                                   :output-to "resources/public/js/main.js"
-                                   :source-map "resources/public/js/main.js.map"
-                                   :optimizations :whitespace
+                        :compiler {:output-to  "resources/public/js/compiled/main.js"
+                                   :output-dir "resources/public/js/compiled/out"
+                                   :source-map true
+                                   :optimizations :none
                                    :pretty-print true}}]}
+  :figwheel {:nrepl-port 7888}
 
   :main ^:skip-aot comic-reader.core
   :target-path "target/%s"
