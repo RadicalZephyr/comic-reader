c6e7a6d2ed840093103f573eb63d6dcfe41cfa2d
Geoff Shannon <geoffpshannon@gmail.com>
2015-04-03 15:49:11 -0500

    Stub out basic re-frame scaffolding

--- src/cljs/comic_reader/main.cljs
+++ src/cljs/comic_reader/main.cljs
@@ -11,3 +11,23 @@
             [reagent.core :as reagent :refer [atom]]
             [reagent.ratom :refer-macros [reaction]]
             [re-frame.core :as rf]))
+
+(defonce time-updater (js/setInterval
+                       #(rf/dispatch [:timer (js/Date.)]) 1000))
+
+(defonce initial-state
+  {:page :sites})
+
+(rf/register-handler
+ :initialize
+ (fn
+   [db _]
+   (merge db initial-state)))
+
+(defn comic-reader []
+  [:div])
+
+(defn ^:export run []
+  (rf/dispatch [:initialize])
+  (reagent/render [comic-reader]
+                  (.-body js/document)))
