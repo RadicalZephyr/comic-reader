18040834ede335d4304edbaacdad2e9da5480f15
Zefira Shannon <earthlingzephyr@gmail.com>
2015-04-03 15:14:44 -0500

    Move figwheel setups into devsrc
    Also, remove secretary based code

---
+++ devsrc/comic_reader/dev.cljs
@@ -0,0 +1,27 @@
+(ns comic-reader.dev
+  (:require [comic-reader.main :as main]
+            [figwheel.client :as fw]))
+
+(enable-console-print!)
+
+;; TODO: Put figwheel setups into some kind of dev namespace
+(fw/start {
+           ;; configure a websocket url if you are using your own server
+           :websocket-url "ws://localhost:3449/figwheel-ws"
+
+           ;; optional callback
+           :on-jsload main/init!
+
+           ;; The heads up display is enabled by default
+           ;; to disable it:
+           ;; :heads-up-display false
+
+           ;; when the compiler emits warnings figwheel
+           ;; blocks the loading of files.
+           ;; To disable this behavior:
+           ;; :load-warninged-code true
+
+           ;; if figwheel is watching more than one build
+           ;; it can be helpful to specify a build id for
+           ;; the client to focus on
+           :build-id "dev"})
--- src/cljs/comic_reader/main.cljs
+++ src/cljs/comic_reader/main.cljs
@@ -1,6 +1,5 @@
 (ns comic-reader.main
-  (:require [figwheel.client :as fw]
-            [comic-reader.api :as api]
+  (:require [comic-reader.api :as api]
             [comic-reader.session :as session]
             [comic-reader.history :as history]
             [comic-reader.pages.sites
@@ -9,54 +8,13 @@
              :refer [comic-list]]
             [comic-reader.pages.viewer
              :refer [comic-viewer]]
-            [reagent.core :as reagent :refer [atom]]
-            [secretary.core :as secretary
-             :refer-macros [defroute]]))
-
-(enable-console-print!)
-
-;; TODO: Put figwheel setups into some kind of dev namespace
-(fw/start {
-           ;; configure a websocket url if you are using your own server
-           :websocket-url "ws://localhost:3449/figwheel-ws"
-
-           ;; optional callback
-           :on-jsload (fn [] (print "reloaded"))
-
-           ;; The heads up display is enabled by default
-           ;; to disable it:
-           ;; :heads-up-display false
-
-           ;; when the compiler emits warnings figwheel
-           ;; blocks the loading of files.
-           ;; To disable this behavior:
-           ;; :load-warninged-code true
-
-           ;; if figwheel is watching more than one build
-           ;; it can be helpful to specify a build id for
-           ;; the client to focus on
-           :build-id "dev"})
-
-
-(defroute "/" []
-  (session/put! :current-page site-list))
-
-(defroute "/site/:site" [site]
-  (session/put! :current-page comic-list))
-
-(defroute "/comic/:comic/:volume/:page" [comic volume page]
-  (session/put! :current-page comic-viewer))
+            [reagent.core :as reagent :refer [atom]]))
 
 (defn page [data]
   [(session/get :current-page) data])
 
 (defn init! []
-  (secretary/set-config! :prefix "#")
-  (session/put! :current-page site-list)
   (api/get-sites
    (fn [data]
      (reagent/render-component [page data]
                                (.-body js/document)))))
-
-(history/hook-browser-navigation!)
-(init!)
