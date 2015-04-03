9e452923f65cecc89cfb49e1519fc9e9e6d0fd1d
Geoff Shannon <geoffpshannon@gmail.com>
2015-04-03 16:12:05 -0500

    Rewrite API call to dispatch a re-frame event

--- src/cljs/comic_reader/api.cljs
+++ src/cljs/comic_reader/api.cljs
@@ -1,10 +1,11 @@
 (ns comic-reader.api
-  (:require [ajax.core :refer [GET POST]]))
+  (:require [re-frame.core :refer [dispatch]]
+            [ajax.core :refer [GET POST]]))
 
 (defn error-handler [{:keys [status status-text]}]
   (.log js/console (str "something bad happened: " status " " status-text)))
 
-(defn get-sites [callback]
-  (GET "/api/v1/sites" {:handler callback
+(defn get-sites []
+  (GET "/api/v1/sites" {:handler #(dispatch [:site-list %])
                         :error-handler error-handler
                         :response-format :edn}))
