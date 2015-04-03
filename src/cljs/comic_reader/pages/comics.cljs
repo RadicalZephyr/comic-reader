ad9b391f24512f5dcb21e94c08ba52019fb9be2d
Geoff Shannon <geoffpshannon@gmail.com>
2015-04-03 16:27:29 -0500

    Remove unused cljs ns's

--- src/cljs/comic_reader/pages/comics.cljs
+++
@@ -1,6 +0,0 @@
-(ns comic-reader.pages.comics
-  (:require [comic-reader.session :as session]
-            [reagent.core :as reagent :refer [atom]]))
-
-(defn comic-list []
-  [:div "Comics!!"])
--- src/cljs/comic_reader/pages/viewer.cljs
+++
@@ -1,6 +0,0 @@
-(ns comic-reader.pages.viewer
-  (:require [comic-reader.session :as session]
-            [reagent.core :as reagent :refer [atom]]))
-
-(defn comic-viewer []
-  [:div "View all the comics..."])
