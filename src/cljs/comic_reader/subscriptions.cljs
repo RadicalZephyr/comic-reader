fc2fb5a32c6390ee32fb4c427781792f0475b513
Zefira Shannon <earthlingzephyr@gmail.com>
2015-04-03 23:52:46 -0500

    Pull the subscriptions out into a separate ns

---
+++ src/cljs/comic_reader/subscriptions.cljs
@@ -0,0 +1,34 @@
+(ns comic-reader.subscriptions
+  (:require [re-frame.core :as rf]
+            [reagent.ratom :refer-macros [reaction]]))
+
+(defn init-subscriptions! []
+  (rf/register-sub
+   :page
+   (fn [db _]
+     (reaction (get @db :page))))
+
+  (rf/register-sub
+   :site-list
+   (fn [db _]
+     (reaction (get @db :site-list))))
+
+  (rf/register-sub
+   :site
+   (fn [db _]
+     (reaction (get @db :site))))
+
+  (rf/register-sub
+   :comic-list
+   (fn [db _]
+     (reaction (get @db :comic-list))))
+
+  (rf/register-sub
+   :comic
+   (fn [db _]
+     (reaction (get @db :comic))))
+
+  (rf/register-sub
+   :location
+   (fn [db _]
+     (reaction (get @db :location)))))
