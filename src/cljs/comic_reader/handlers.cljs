703964700a6af90f371d8dbfc9e2f7084e84b9e1
Zefira Shannon <earthlingzephyr@gmail.com>
2015-04-03 23:50:58 -0500

    Separate handlers out into a separate ns

---
+++ src/cljs/comic_reader/handlers.cljs
@@ -0,0 +1,30 @@
+(ns comic-reader.handlers
+  (:require [re-frame.core :as rf]))
+
+(defonce initial-state
+  {:page :sites})
+
+(defn init-handlers! []
+  (rf/register-handler
+   :initialize
+   (fn [db _]
+     (merge db initial-state)))
+
+  (rf/register-handler
+   :sites
+   (fn [db [page]]
+     (assoc db :page page)))
+
+  (rf/register-handler
+   :comics
+   (fn [db [page site]]
+     (-> db
+         (assoc :page page)
+         (assoc :site site))))
+
+  (rf/register-handler
+   :read
+   (fn [db [page location]]
+     (-> db
+         (assoc :page page)
+         (assoc :location location)))))
