bbb88177c259cbfdccc7bec9c5dee761501a7a1e
Zefira Shannon <earthlingzephyr@gmail.com>
2015-04-04 15:54:40 -0500

    Abstract out turning hiccup link data into simple maps

--- src/clj/comic_reader/sites.clj
+++ src/clj/comic_reader/sites.clj
@@ -2,6 +2,14 @@
   (:refer-clojure :exclude [list])
   (:require [clojure.string :as s]))
 
+(defn gen-link->map [process-name process-url]
+  (fn [{[name] :content
+        {url :href} :attrs}]
+    {:name (process-name name)
+     :url (process-url url)}))
+
+(def simple-link->map (gen-link->map identity identity))
+
 (def list
   [(let [canonical-url "http://mangafox.me"
          manga-url (format "%s/manga/" canonical-url)
@@ -10,31 +18,27 @@
       :name "Manga Fox"
       :url manga-url
       :selector [:div.manga_list :ul :li :a]
-      :normalize (fn [{[name] :content
-                       {url :href} :attrs}]
-                   {:name name
-                    :url url})
+      :normalize simple-link->map
       :chapter-list-data (fn [chapter-url]
                            {:url chapter-url
                             :selector [:div#chapters :ul.chlist
                                        :li :div :h3 :a]
-                            :normalize (fn [data] data)})
+                            :normalize simple-link->map})
       :url->feed (fn [url]
                    (some->> url
                             (re-matches manga-pattern)
                             second
                             (format "%s/rss/%s.xml" canonical-url)))})
 
-   (let [canonical-url "http://www.mangareader.net/"]
+   (let [canonical-url "http://www.mangareader.net/"
+         link->map (gen-link->map s/trim
+                                  (partial str canonical-url))]
      {:id :manga-reader
       :name "Manga Reader"
       :url (str canonical-url "alphabetical")
       :selector [:div.series_alpha :ul :li :a]
-      :normalize (fn [{[name] :content
-                       {url :href} :attrs}]
-                   {:name (s/trim name)
-                    :url (str canonical-url url)})
+      :normalize link->map
       :chapter-list-data (fn [chapter-url]
                            {:url chapter-url
                             :selector [:div#chapterlist :tr :td :a]
-                            :normalize (fn [data] data)})})])
+                            :normalize link->map})})])
