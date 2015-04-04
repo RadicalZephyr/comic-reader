044e70f8dea956f7bf6a3c0ab07ca4dc6903418f
Zefira Shannon <earthlingzephyr@gmail.com>
2015-04-04 18:20:53 -0500

    Add function for getting a hiccup image tag

--- src/clj/comic_reader/scrape.clj
+++ src/clj/comic_reader/scrape.clj
@@ -12,3 +12,14 @@
 (defn fetch-list [{:keys [url selector normalize]}]
   (when (every? (complement nil?) [url selector normalize])
     (map normalize (html/select (fetch-url url) selector))))
+
+(defn enlive->hiccup [{:keys [tag attrs content]}]
+  (if (nil? content)
+    [tag attrs]
+    [tag attrs content]))
+
+(defn fetch-image-tag [url]
+  (-> (fetch-url url)
+      (html/select [:div#imgholder :a :img#img])
+      first
+      enlive->hiccup))
