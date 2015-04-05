2eae1f87b51975eebcf1ba891b4c9389ece46b77
Zefira Shannon <earthlingzephyr@gmail.com>
2015-04-05 01:40:41 -0500

    Add a scrolling ns
    Setup scrolling to trigger a throttled event.  Translation from the
    example on this MDN page:
    https://developer.mozilla.org/en-US/docs/Web/Events/scroll

---
+++ src/cljs/comic_reader/scrolling.cljs
@@ -0,0 +1,18 @@
+(ns comic-reader.scrolling
+  (:require [re-frame.core :as rf]))
+
+(defn throttle [type key target]
+  (let [target (or target js/window)
+        running (atom false)
+        toggle! #(swap! running not)
+        throttler (fn []
+                    (when (not @running)
+                      (toggle!)
+                      (js/requestAnimationFrame
+                       (fn []
+                         (rf/dispatch [key])
+                         (toggle!)))))]
+    (.addEventListener target type throttler)))
+
+(defn setup-scrolling-events! []
+  (throttle "scroll" :scroll))
