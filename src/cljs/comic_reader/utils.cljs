dc8290511416573e4991316708f472ca4282d0d0
Zefira Shannon <earthlingzephyr@gmail.com>
2015-04-06 21:26:43 -0500

    Add a utils ns in cljs

---
+++ src/cljs/comic_reader/utils.cljs
@@ -0,0 +1,11 @@
+(ns comic-reader.utils
+  (:require [clojure.string :as s]))
+
+(defn titlize [title & {:keys [to-spaces]
+                        :or {to-spaces #" "}}]
+  (->>
+   (-> title
+       (s/replace to-spaces " ")
+       (s/split #" "))
+   (map s/capitalize)
+   (s/join " ")))
