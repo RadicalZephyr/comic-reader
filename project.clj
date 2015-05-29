8c7ace4c81ffab8560586e3c7bfd67eb1b4fd24f
Geoff Shannon <geoffpshannon@gmail.com>
2015-05-29 08:23:21 -0500

    Update ring to 1.4.0-RC1

--- project.clj
+++ project.clj
@@ -9,7 +9,7 @@
   :source-paths ["src/clj"]
 
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [ring "1.3.2"]
+                 [ring "1.4.0-RC1"]
                  [fogus/ring-edn "0.2.0"]
                  [compojure "1.3.4"]
                  [hiccup "1.0.5"]
