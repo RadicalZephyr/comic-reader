1d5fc97c110a24dc025ec2b432d09f8bd6f1dfd9
Zefira Shannon <earthlingzephyr@gmail.com>
2015-09-01 22:46:35 -0500

    Test extract-pages-list

--- test/comic_reader/sites/manga_fox_test.clj
+++ test/comic_reader/sites/manga_fox_test.clj
@@ -11,3 +11,16 @@
     (is (= (extract-image-tag html)
            [:img {:src "http://a.mfcdn.net/store/manga/13088/02-095.0/compressed/m001.jpg"
                   :alt "The Gamer 95 at MangaFox.me"}]))))
+
+(deftest extract-pages-list-test
+  (let [html (html/html [:div {} [:img {}]])
+        pages (extract-pages-list html "root.url")]
+    (is (= (seq pages)
+           nil)))
+
+  (let [html (html/html-resource "test/manga_fox/image.html")
+        pages (extract-pages-list html "root.url")]
+    (is (= (count pages)
+           24))
+    (is (= (first pages)
+           {:name "1" :url "root.url/1.html"}))))
