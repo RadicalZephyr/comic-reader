f2abcc22b85224102b1d9f3e531564c2abfdb396
Geoff Shannon <geoffpshannon@gmail.com>
2015-09-01 22:46:35 -0500

    Test extract-pages-list

--- src/clj/comic_reader/sites/manga_fox.clj
+++ src/clj/comic_reader/sites/manga_fox.clj
@@ -1,11 +1,27 @@
 (ns comic-reader.sites.manga-fox
   (:require [comic-reader.sites.protocol :refer [MangaSite]]
-            [comic-reader.scrape :as scrape]))
+            [comic-reader.scrape :as scrape]
+            [clojure.string :as s]))
+
+(def ^:private root-url "http://mangafox.me")
 
 (def ^:private image-selector [:div#viewer :img#image])
 
 (defn extract-image-tag [html]
   (scrape/extract-image-tag html image-selector))
+
+(def ^:private page-list-selector
+  [:div#top_center_bar :form#top_bar :select.m :option])
+
+(defn extract-pages-list [html chapter-url]
+  (let [base-url (s/replace chapter-url
+                            #"/\d+\.html"
+                            "")
+        normalize (fn [{{name :value} :attrs}]
+                    {:name name
+                     :url (format "%s/%s.html"
+                                  base-url name)})]
+    (scrape/extract-list html page-list-selector normalize)))
 
 (deftype MangaFox []
   MangaSite
