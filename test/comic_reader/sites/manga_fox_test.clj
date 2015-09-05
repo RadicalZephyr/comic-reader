15eee495ec82027c323ba1db634257810deb441e
Zefira Shannon <earthlingzephyr@gmail.com>
2015-09-05 00:54:51 -0500

    Move integration tests into their own ns

--- test/comic_reader/sites/manga_fox_test.clj
+++ test/comic_reader/sites/manga_fox_test.clj
@@ -56,19 +56,3 @@
            {:name "-6mm no Taboo",
             :url "http://mangafox.me/manga/6mm_no_taboo/",
             :id "6mm_no_taboo"}))))
-
-(deftest ^:integration get-comics-list-test
-  (let [comics (get-comic-list manga-fox)]
-    (is (> (count comics)
-           15000))
-    (let [comic-6mm {:name "-6mm no Taboo",
-                     :url "http://mangafox.me/manga/6mm_no_taboo/",
-                     :id "6mm_no_taboo"}]
-      (is (some #{comic-6mm}
-                comics)
-          comic-6mm))))
-
-(deftest ^:integration get-chapter-list-test
-  (let [chapters (get-chapter-list manga-fox "the_gamer")]
-    (is (>= (count chapters)
-            97))))
