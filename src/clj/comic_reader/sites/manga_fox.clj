ef20620afadbb04608670f4efc9757f8d0ab6e0f
Zefira Shannon <earthlingzephyr@gmail.com>
2015-09-11 22:46:58 -0500

    Inline usage of gen-add-key-from-url

--- src/clj/comic_reader/sites/manga_fox.clj
+++ src/clj/comic_reader/sites/manga_fox.clj
@@ -84,23 +84,31 @@
                          (page-list-selector)
                          normalize)))
 
-(def ^:private chapter-link-normalize
-  (comp
-   (util/gen-add-key-from-url :ch-num
-                              (chapter-number-match-pattern)
-                              safe-read-string)
-   link->map))
+(defn chapter-link-add-ch-num [{:keys [url]
+                            :as comic-map}]
+  (let [[_ data] (re-find (chapter-number-match-pattern) url)]
+    (assoc comic-map
+           :ch-num (safe-read-string data))))
+
+(defn chapter-link-normalize [link]
+  (-> link
+      link->map
+      chapter-link-add-ch-num))
 
 (defn extract-chapters-list [html comic-url]
   (scrape/extract-list html
                        (chapter-list-selector)
                        chapter-link-normalize))
 
-(def ^:private comic-link-normalize
-  (comp
-   (util/gen-add-key-from-url :id
-                              (manga-pattern))
-   link->map))
+(defn comic-link-add-id [{:keys [url] :as comic-map}]
+  (let [[_ data] (re-find (manga-pattern) url)]
+    (assoc comic-map
+           :id data)))
+
+(defn comic-link-normalize [link]
+  (-> link
+      link->map
+      comic-link-add-id))
 
 (defn extract-comics-list [html]
   (scrape/extract-list html
