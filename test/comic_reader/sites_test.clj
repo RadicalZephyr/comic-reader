8809000342251f882c51f9151df4b441ae13435c
Zefira Shannon <earthlingzephyr@gmail.com>
2015-09-13 04:44:14 -0500

    Add tests for utility functions in sites

--- test/comic_reader/sites_test.clj
+++ test/comic_reader/sites_test.clj
@@ -5,6 +5,37 @@
             [net.cgrand.enlive-html :as html]
             [clj-http.client :as client]))
 
+(deftest base-name-test
+  (is (= (base-name "abc.123")
+         "abc"))
+  (is (= (base-name "thingy.clj")
+         "thingy"))
+  (is (= (base-name "thing.part.two.clj")
+         "thing.part.two"))
+  (is (= (base-name "dir/two/three/four.clj")
+         "four")))
+
+(deftest get-all-sites-test
+  (is (= (get-all-sites)
+         ["manga-fox"])))
+
+(deftest read-site-options-test
+  (is (thrown?
+       java.lang.IllegalArgumentException
+       (read-site-options "non-existent")))
+  (is (= (class (read-site-options "manga-fox"))
+         clojure.lang.PersistentHashMap)))
+
+(deftest make-site-entry-test
+  (is (= (make-site-entry "non-existent")
+         [nil nil]))
+
+  (let [[label opts] (make-site-entry "manga-fox")]
+    (is (= label
+           "manga-fox"))
+    (is (= (class opts)
+           comic_reader.sites.MangaSite))))
+
 (defmacro test-data-functions-not-nil []
   `(are [selector] (is (not= (selector)
                              nil))
