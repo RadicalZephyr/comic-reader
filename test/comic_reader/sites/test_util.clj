1cf6a3ad93acbab8feb39f266e9640b426f0b1a5
Zefira Shannon <earthlingzephyr@gmail.com>
2015-09-13 06:09:15 -0500

    Reimplement test all data functions
    Use the set of all data function keywords as a source

--- test/comic_reader/sites/test_util.clj
+++ test/comic_reader/sites/test_util.clj
@@ -5,38 +5,6 @@
             [loom.graph :as graph]
             [loom.alg :as alg]))
 
-(defmacro test-data-functions-not-nil []
-  `(are [selector] (is (not= (selector)
-                             nil))
-     root-url
-     manga-list-format
-     manga-url-format
-     manga-pattern-match-portion
-
-     comic->url-format
-
-     chapter-list-selector
-     comic-list-selector
-     image-selector
-     page-list-selector
-
-     chapter-number-pattern
-     chapter-number-match-pattern
-
-     link-name-normalize
-     link-url-normalize
-
-     page-normalize-format
-     page-normalize-pattern))
-
-(defmacro test-url-format-strings []
-  `(are [url-fn] (is (= (:status (client/head (url-fn)))
-                        200)
-                     (str (url-fn) "does not appear to exist."))
-     root-url
-     manga-url
-     manga-list-url))
-
 (def dependecy-dag
   (graph/digraph
    [:get-image-data :extract-image-tag]
@@ -112,10 +80,27 @@
     :page-normalize-format
     :page-normalize-pattern})
 
+(def key->sym
+  (comp symbol name))
+
+(defmacro test-data-functions-not-nil []
+  `(are [selector] (is (not= (selector)
+                             nil))
+     ~@(->> (seq data-function?)
+            (map key->sym))))
+
+(defn test-url-format-strings []
+  (are [url-fn] (is (= (:status (client/head (url-fn)))
+                       200)
+                    (str (url-fn) "does not appear to exist."))
+    root-url
+    manga-url
+    manga-list-url))
+
 (defmacro ensure-dependencies-defined [fn-name]
   `(are [selector] (is (not= (selector)
                              nil))
      ~@(->> (keyword fn-name)
             (alg/topsort dependecy-dag)
             (filter data-function?)
-            (map (comp symbol name)))))
+            (map key->sym))))
