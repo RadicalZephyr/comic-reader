b74b0078e104a216cb141558022d6d227be2ba98
Zefira Shannon <earthlingzephyr@gmail.com>
2015-09-21 20:34:50 -0500

    Improve site-tests
    - Make the site-name a dynamically bound variable instead of passing it
      everywhere
    - Factor out a resource-exists? function
    - Add a test for the presence of the image.html file

--- test/comic_reader/sites_test.clj
+++ test/comic_reader/sites_test.clj
@@ -1,7 +1,9 @@
 (ns comic-reader.sites-test
   (:require [clojure.test :refer :all]
             [comic-reader.sites :refer :all]
-            [clojure.java.io :as io]))
+            [comic-reader.sites.test-util :as tu]
+            [clojure.java.io :as io]
+            [net.cgrand.enlive-html :as html]))
 
 (deftest base-name-test
   (is (= (base-name "abc.123")
@@ -44,29 +46,48 @@
           (str "Contents of `sites/" site ".clj'"
                " cannot be empty")))))
 
-(defn site-test-folder [site-name]
+(def ^:dynamic site-name)
+
+(defn site-test-folder []
   (format "test/%s" site-name))
 
-(defn has-test-folder? [site-name]
-  (some->
-   site-name
-   site-test-folder
-   io/resource
-   io/as-file
-   .exists))
+(defn resource-exists? [path]
+  (some-> path
+          io/resource
+          io/as-file
+          .exists))
 
-(defn error-must-have-test-data [site-name]
+(defn has-test-folder? []
+  (some-> (site-test-folder)
+          resource-exists?))
+
+(defn error-must-have-test-data []
   (is false
       (str "There must be a site test data folder at "
            "`resources/test/" site-name "'")))
 
+(defn image-page-html []
+  (let [image-path (format "%s/image.html" (site-test-folder))]
+    (if (resource-exists? image-path)
+      (html/html-resource image-path)
+      nil)))
+
+(defn test-extract-image-tag []
+  (if-let [html (image-page-html)]
+    (do)
+    (is false
+        (str "There must be a sample image html page at "
+             "`resources/test/" site-name "/image.html'"))))
+
 (defn testdef-form [site-name]
   `(deftest ~(symbol (str site-name "-test"))
-     (expect-opts-are-map ~site-name)
-     (if (has-test-folder? ~site-name)
-       (do
-         )
-       (error-must-have-test-data ~site-name))))
+     (binding [~'site-name ~site-name]
+       (expect-opts-are-map ~site-name)
+       (if (has-test-folder?)
+         (do
+           (test-extract-image-tag)
+           )
+         (error-must-have-test-data)))))
 
 (defmacro defsite-tests []
   (try
