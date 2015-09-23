7ac1a884ae2d4a7a0fa99c24ad1332c0db567b06
Geoff Shannon <geoffpshannon@gmail.com>
2015-09-22 23:51:25 -0500

    Implement tests for extract-pages-list

--- resources/test/manga-fox/image.clj
+++ resources/test/manga-fox/image.clj
@@ -1,4 +1,28 @@
 {:image-tag [:img
              {:src
               "http://a.mfcdn.net/store/manga/13088/02-095.0/compressed/m001.jpg"
-              :alt "The Gamer 95 at MangaFox.me"}]}
+              :alt "The Gamer 95 at MangaFox.me"}]
+
+ :pages-list [{:name "1", :url "/1.html"}
+              {:name "2", :url "/2.html"}
+              {:name "3", :url "/3.html"}
+              {:name "4", :url "/4.html"}
+              {:name "5", :url "/5.html"}
+              {:name "6", :url "/6.html"}
+              {:name "7", :url "/7.html"}
+              {:name "8", :url "/8.html"}
+              {:name "9", :url "/9.html"}
+              {:name "10", :url "/10.html"}
+              {:name "11", :url "/11.html"}
+              {:name "12", :url "/12.html"}
+              {:name "13", :url "/13.html"}
+              {:name "14", :url "/14.html"}
+              {:name "15", :url "/15.html"}
+              {:name "16", :url "/16.html"}
+              {:name "17", :url "/17.html"}
+              {:name "18", :url "/18.html"}
+              {:name "19", :url "/19.html"}
+              {:name "20", :url "/20.html"}
+              {:name "21", :url "/21.html"}
+              {:name "22", :url "/22.html"}
+              {:name "23", :url "/23.html"}]}
