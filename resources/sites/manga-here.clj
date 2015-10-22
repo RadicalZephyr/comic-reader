f98c35e2571b8d5586d005cdd6b9b025db471339
Zefira Shannon <earthlingzephyr@gmail.com>
2015-10-21 22:32:55 -0500

    First try at a site definition for manga-here

---
+++ resources/sites/manga-here.clj
@@ -0,0 +1,23 @@
+{:image-selector [section#viewer.read_img]
+ :manga-list-format "%s/mangalist/"
+ :comic-list-selector [div.list_manga ul li a.manga_info]
+
+ :page-normalize-pattern #".*"
+ :page-normalize-format "%sc%3d/"
+
+ :chapter-number-pattern #"c0*\d*/"
+
+ :page-list-selector [select#top_chapter_list option]
+
+ :root-url "http://www.mangahere.co/"
+ :manga-url-format "%s/manga/"
+
+ :manga-url-suffix-pattern #"(.*?)/c\d*/$"
+
+ :link-name-normalize first
+ :link-url-normalize identity
+
+ :chapter-list-selector [div.manga_detail div.detail_list ul li span.left a]
+ :chapter-number-match-pattern #"c0*(\d*)/"
+
+ :comic->url-format "%s%s/"}
