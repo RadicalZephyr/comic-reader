1eb894b3ea9411be22712fb6fac74e206920d974
Geoff Shannon <geoffpshannon@gmail.com>
2015-10-21 22:32:55 -0500

    First try at a site definition for manga-here

---
+++ resources/test/manga-here/image.clj
@@ -0,0 +1,26 @@
+{:image-tag [:img
+             {:src "http://a.mhcdn.net/store/manga/13739/095.0/compressed/rthe-gamer-5978533.jpg?v=1440085982"
+              :alt "The Gamer 95 Page 1"}]
+ :pages-list [{:name "1", :url  "http://www.mangahere.co/manga/the_gamer/c095/"}
+              {:name "2", :url  "http://www.mangahere.co/manga/the_gamer/c095/2"}
+              {:name "3", :url  "http://www.mangahere.co/manga/the_gamer/c095/3"}
+              {:name "4", :url  "http://www.mangahere.co/manga/the_gamer/c095/4"}
+              {:name "5", :url  "http://www.mangahere.co/manga/the_gamer/c095/5"}
+              {:name "6", :url  "http://www.mangahere.co/manga/the_gamer/c095/6"}
+              {:name "7", :url  "http://www.mangahere.co/manga/the_gamer/c095/7"}
+              {:name "8", :url  "http://www.mangahere.co/manga/the_gamer/c095/8"}
+              {:name "9", :url  "http://www.mangahere.co/manga/the_gamer/c095/9"}
+              {:name "10", :url "http://www.mangahere.co/manga/the_gamer/c095/10"}
+              {:name "11", :url "http://www.mangahere.co/manga/the_gamer/c095/11"}
+              {:name "12", :url "http://www.mangahere.co/manga/the_gamer/c095/12"}
+              {:name "13", :url "http://www.mangahere.co/manga/the_gamer/c095/13"}
+              {:name "14", :url "http://www.mangahere.co/manga/the_gamer/c095/14"}
+              {:name "15", :url "http://www.mangahere.co/manga/the_gamer/c095/15"}
+              {:name "16", :url "http://www.mangahere.co/manga/the_gamer/c095/16"}
+              {:name "17", :url "http://www.mangahere.co/manga/the_gamer/c095/17"}
+              {:name "18", :url "http://www.mangahere.co/manga/the_gamer/c095/18"}
+              {:name "19", :url "http://www.mangahere.co/manga/the_gamer/c095/19"}
+              {:name "20", :url "http://www.mangahere.co/manga/the_gamer/c095/20"}
+              {:name "21", :url "http://www.mangahere.co/manga/the_gamer/c095/21"}
+              {:name "22", :url "http://www.mangahere.co/manga/the_gamer/c095/22"}
+              {:name "23", :url "http://www.mangahere.co/manga/the_gamer/c095/23"}]}
