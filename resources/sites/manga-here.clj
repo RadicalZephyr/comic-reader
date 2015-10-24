1ecd0a480feb0599bd788d971572e72131a313d2
Zefira Shannon <earthlingzephyr@gmail.com>
2015-10-23 22:43:41 -0500

    Correct manga-here selectors

--- resources/sites/manga-here.clj
+++ resources/sites/manga-here.clj
@@ -1,4 +1,4 @@
-{:image-selector [section#viewer.read_img]
+{:image-selector [:section#viewer.read_img :a :img]
  :manga-list-format "%s/mangalist/"
  :comic-list-selector [div.list_manga ul li a.manga_info]
 
@@ -7,7 +7,7 @@
 
  :chapter-number-pattern #"c0*\d*/"
 
- :page-list-selector [select#top_chapter_list option]
+ :page-list-selector [:select.wid60 :option]
 
  :root-url "http://www.mangahere.co/"
  :manga-url-format "%s/manga/"
@@ -17,7 +17,7 @@
  :link-name-normalize first
  :link-url-normalize identity
 
- :chapter-list-selector [div.manga_detail div.detail_list ul li span.left a]
+ :chapter-list-selector [:div.manga_detail :div.detail_list :ul :li :span.left :a]
  :chapter-number-match-pattern #"c0*(\d*)/"
 
  :comic->url-format "%s%s/"}
