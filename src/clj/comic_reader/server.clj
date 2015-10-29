74779c92c438b64ae76fa0859448f8e9f9c657ee
Geoff Shannon <geoffpshannon@gmail.com>
2015-10-28 22:38:31 -0500

    Gut the server ns

--- src/clj/comic_reader/server.clj
+++ src/clj/comic_reader/server.clj
@@ -1,74 +1,34 @@
 (ns comic-reader.server
   (:gen-class)
-  (:require [comic-reader.sites.protocol :refer :all]
-            [comic-reader.sites :refer [sites]]
-            [comic-reader.scrape :as scrape]
-            [comic-reader.util :refer [safe-read-string]]
-            [ring.util.response :as response]
-            [ring.middleware.params :refer [wrap-params]]
-            [ring.middleware.edn :refer [wrap-edn-params]]
-            [compojure.core  :as c]
+  (:require [compojure.core :as c]
             [compojure.route :as route]
-            [hiccup.page :as hp]
             [environ.core :refer [env]]
-            [ring.adapter.jetty :refer [run-jetty]]))
+            [hiccup.page :as page]
+            [ring.adapter.jetty :refer [run-jetty]]
+            [ring.middleware.edn :refer [wrap-edn-params]]
+            [ring.middleware.params :refer [wrap-params]]))
 
 (defn edn-response [data & [status]]
   {:status (or status 200)
    :headers {"Content-Type" "application/edn"}
    :body (pr-str data)})
 
-(defn gen-error-data [request & {:as others}]
-  (-> request
-      (select-keys [:uri
-                    :request-method
-                    :params])
-      (merge others)))
-
-(defn get-comics-list [{{:keys [site]} :params
-                        :as request}]
-  )
-
-(defn get-comic-chapter [site comic chapter]
-  )
-
-(defn get-following-pages [site comic chapter-map page]
-  )
-
-(defn get-comic-pages [{{:keys [site comic chapter page]} :params
-                        :as request}]
-  (let [site (keyword site)
-        chapter (safe-read-string chapter)
-        page    (safe-read-string page)
-        add-location (fn [idx comic-info]
-                       (assoc comic-info
-                              :chapter chapter
-                              :page (+ page idx)))
-        chapter-map (get-comic-chapter site comic chapter)
-        page-list (get-following-pages site comic
-                                       chapter-map
-                                       page)]
-    (if-let [comic-pages (map-indexed add-location
-                                      page-list)]
-      (edn-response comic-pages)
-      (edn-response (gen-error-data request) 404))))
-
 (c/defroutes routes
-  (c/GET "/" [] (hp/html5
+  (c/GET "/" [] (page/html5
                  [:head
-                  (hp/include-css "css/normalize.css"
-                                  "css/foundation.min.css"
-                                  "css/app.css")
-                  (hp/include-js "js/vendor/modernizr.js")]
+                  (page/include-css "css/normalize.css"
+                                    "css/foundation.min.css"
+                                    "css/app.css")
+                  (page/include-js "js/vendor/modernizr.js")]
                  [:body
                   [:div.row
                    [:div#app.small-12.columns]]
                   [:input#history_state {:type "hidden"}]
-                  (hp/include-js "js/vendor/jquery.js"
-                                 "js/vendor/fastclick.js"
-                                 "js/foundation.min.js"
-                                 "js/compiled/main.js")]))
-  (c/GET "/blank" [] "")
+                  (page/include-js "js/vendor/jquery.js"
+                                   "js/vendor/fastclick.js"
+                                   "js/foundation.min.js"
+                                   "js/compiled/main.js")]))
+
   (c/context "/api/v1" []
     (c/GET "/sites" []
       (edn-response
