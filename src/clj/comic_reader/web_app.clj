4107dc2dfcb52f7048e689676da6d43217c173b7
Geoff Shannon <geoffpshannon@gmail.com>
2015-10-30 00:07:38 -0500

    Extract routes into a separate component
    This is mainly for ease of testing. It seems very wrong to start up a
    webserver to run mock request tests against the compojure routes.

---
+++ src/clj/comic_reader/web_app.clj
@@ -0,0 +1,69 @@
+(ns comic-reader.web-app
+  (:require [compojure.core :as c]
+            [compojure.route :as route]
+            [com.stuartsierra.component :as component]
+            [hiccup.page :as page]
+            [ring.middleware.edn :refer [wrap-edn-params]]
+            [ring.middleware.params :refer [wrap-params]]))
+
+(defn edn-response [data & [status]]
+  {:status (or status 200)
+   :headers {"Content-Type" "application/edn"}
+   :body (pr-str data)})
+
+(defn make-api-routes [site-scraper]
+  (c/routes
+    (c/GET "/sites" []
+      (edn-response ()
+                    ))
+
+    (c/GET "/comics/:site" [site]
+      )
+
+    (c/GET "/pages/:site/:comic/:chapter{\\d+}/:page{\\d+}"
+        request
+      )
+
+    (c/POST "/img" {{:keys [site]
+                     {:keys [chapter page url]} :page-info}
+                    :edn-params
+                    :as request}
+      )))
+
+(defn make-routes [site-scraper]
+  (c/routes
+    (c/GET "/" []
+      (page/html5
+       [:head
+        (page/include-css "css/normalize.css"
+                          "css/foundation.min.css"
+                          "css/app.css")
+        (page/include-js "js/vendor/modernizr.js")]
+       [:body
+        [:div.row
+         [:div#app.small-12.columns]]
+        [:input#history_state {:type "hidden"}]
+        (page/include-js "js/vendor/jquery.js"
+                         "js/vendor/fastclick.js"
+                         "js/foundation.min.js"
+                         "js/compiled/main.js")]))
+
+    (c/context "/api/v1" []
+      (-> (make-api-routes site-scraper)
+          wrap-params
+          wrap-edn-params))
+
+    (route/resources "/")))
+
+(defrecord WebApp [routes site-scraper]
+  component/Lifecycle
+
+  (start [component]
+    (println "Generating web app...")
+    (assoc component :routes (make-routes site-scraper)))
+
+  (stop [component]
+    (assoc component :routes nil)))
+
+(defn new-web-app []
+  (map->WebApp {}))
