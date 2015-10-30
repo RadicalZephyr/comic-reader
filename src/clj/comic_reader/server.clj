4d2fa159e6ffa43a7e0d75044f17ffee6985a1eb
Zefira Shannon <earthlingzephyr@gmail.com>
2015-10-29 19:47:55 -0500

    Create a WebServer component

--- src/clj/comic_reader/server.clj
+++ src/clj/comic_reader/server.clj
@@ -2,11 +2,13 @@
   (:gen-class)
   (:require [compojure.core :as c]
             [compojure.route :as route]
+            [com.stuartsierra.component :as component]
             [environ.core :refer [env]]
             [hiccup.page :as page]
             [ring.adapter.jetty :refer [run-jetty]]
             [ring.middleware.edn :refer [wrap-edn-params]]
-            [ring.middleware.params :refer [wrap-params]]))
+            [ring.middleware.params :refer [wrap-params]])
+  (:import org.eclipse.jetty.server.Server))
 
 (defn edn-response [data & [status]]
   {:status (or status 200)
@@ -52,6 +54,32 @@
 (def app (-> routes
              wrap-params
              wrap-edn-params))
+
+(defrecord WebServer [app port ^Server server site-scraper]
+  component/Lifecycle
+
+  (start [component]
+    (if server
+      component
+      (do
+        (println "Starting web server...")
+        (assoc component
+               :server (run-jetty app {:port port
+                                       :join? false})))))
+
+  (stop [component]
+    (if (not server)
+      component
+      (do
+        (when (or (not (.isStopped server))
+                  (not (.isStopping server)))
+          (println "Shutting down web server...")
+          (.stop server))
+        (assoc component :server nil)))))
+
+(defn new-server [app port]
+  (map->WebServer {:app app
+                   :port port}))
 
 (defn run-web-server [& [port]]
   (let [port (Integer. (or port (env :port) 10555))]
