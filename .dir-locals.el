f4d147acb3e81ca1266d4eee3db4128fdb7d4694
Geoff Shannon <geoffpshannon@gmail.com>
2015-10-29 21:36:45 -0500

    Add emacs dir local variables for refreshing

---
+++ .dir-locals.el
@@ -0,0 +1,7 @@
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+((cider-repl-mode
+  (cider-refresh-before-fn . "comic-reader.system/stop"))
+ (clojure-mode
+  (cider-refresh-before-fn . "comic-reader.system/stop")))
