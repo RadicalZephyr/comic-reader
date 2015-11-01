e6e3f3b61be6c53cd7240502cd33c751055da89e
Geoff Shannon <geoffpshannon@gmail.com>
2015-11-01 15:26:26 -0600

    Remove datomic setups and simplify deps

--- src/clj/comic_reader/comic_repository.clj
+++ src/clj/comic_reader/comic_repository.clj
@@ -1,27 +1,28 @@
 (ns comic-reader.comic-repository
   (:require [com.stuartsierra.component :as component]
-            [datomic.api :as d]))
+;;            [datomic.api :as d]
+            ))
 
 (def db-uri "datomic:mem://comic-reader")
 
-(defrecord ComicRepository [db-uri connection comic-scraper]
-  component/Lifecycle
+;; (defrecord ComicRepository [db-uri connection comic-scraper]
+;;   component/Lifecycle
 
-  (start [component]
-    (if connection
-      component
-      (do (d/create-database db-uri)
+;;   (start [component]
+;;     (if connection
+;;       component
+;;       (do (d/create-database db-uri)
 
-          (println "Connecting to database...")
-          (let [conn (d/connect db-uri)]
-            (assoc component :connection conn)))))
+;;           (println "Connecting to database...")
+;;           (let [conn (d/connect db-uri)]
+;;             (assoc component :connection conn)))))
 
-  (stop [component]
-    (if (not connection)
-      component
-      (do (println "Releasing database connection...")
-          (d/release connection)
-          (assoc component :connection nil)))))
+;;   (stop [component]
+;;     (if (not connection)
+;;       component
+;;       (do (println "Releasing database connection...")
+;;           (d/release connection)
+;;           (assoc component :connection nil)))))
 
-(defn new-repository [db-uri]
-  (map->ComicRepository {:db-uri db-uri}))
+;; (defn new-repository [db-uri]
+;;   (map->ComicRepository {:db-uri db-uri}))
