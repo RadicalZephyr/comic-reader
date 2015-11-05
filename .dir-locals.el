d7cd2a2e74c80175c7ac0a2cf41917fbd6d4cb49
Zefira Shannon <earthlingzephyr@gmail.com>
2015-11-04 22:08:05 -0600

    Remove problematic cider-before-fn setting

--- .dir-locals.el
+++
@@ -1,7 +0,0 @@
-;;; Directory Local Variables
-;;; For more information see (info "(emacs) Directory Variables")
-
-((cider-repl-mode
-  (cider-refresh-before-fn . "comic-reader.system/stop"))
- (clojure-mode
-  (cider-refresh-before-fn . "comic-reader.system/stop")))
