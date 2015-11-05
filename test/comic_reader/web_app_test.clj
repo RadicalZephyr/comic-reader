daa1c7cd513746237bf36a4c15ed9a2cd187c367
Geoff Shannon <geoffpshannon@gmail.com>
2015-11-04 21:55:00 -0600

    Add a test for the existence of web root

--- test/comic_reader/web_app_test.clj
+++ test/comic_reader/web_app_test.clj
@@ -1,5 +1,6 @@
 (ns comic-reader.web-app-test
   (:require [clojure.test :refer :all]
+            [comic-reader.site-scraper :as site-scraper]
             [comic-reader.web-app :refer :all]
             [com.stuartsierra.component :as component]
             [ring.mock.request :as mock]))
@@ -10,3 +11,15 @@
    :web-app (component/using
              (new-web-app)
              [:site-scraper])))
+
+(defn test-system []
+  (-> (server-test-system)
+      (component/start)))
+
+(defn app-routes [system]
+  (get-routes (:web-app system)))
+
+(deftest test-home-page
+  (is (= (:status
+          ((app-routes (test-system)) (mock/request :get "/")))
+         200)))
