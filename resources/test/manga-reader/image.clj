8159fa51e6bfa4e8b923b7f91384513100faede5
Zefira Shannon <earthlingzephyr@gmail.com>
2015-11-08 21:33:34 -0600

    Start working on manga-reader site definition

---
+++ resources/test/manga-reader/image.clj
@@ -0,0 +1,29 @@
+{:image-tag [:img
+             {:alt "The Gamer 95 - Page 1",
+              :src
+              "http://i995.mangareader.net/the-gamer/95/the-gamer-5978527.jpg"}]
+ :chapter-url "base.url"
+ :pages-list [{:name "1", :url "base.url/"}
+              {:name "2", :url "base.url/"}
+              {:name "3", :url "base.url/"}
+              {:name "4", :url "base.url/"}
+              {:name "5", :url "base.url/"}
+              {:name "6", :url "base.url/"}
+              {:name "7", :url "base.url/"}
+              {:name "8", :url "base.url/"}
+              {:name "9", :url "base.url/"}
+              {:name "10", :url "base.url/"}
+              {:name "11", :url "base.url/"}
+              {:name "12", :url "base.url/"}
+              {:name "13", :url "base.url/"}
+              {:name "14", :url "base.url/"}
+              {:name "15", :url "base.url/"}
+              {:name "16", :url "base.url/"}
+              {:name "17", :url "base.url/"}
+              {:name "18", :url "base.url/"}
+              {:name "19", :url "base.url/"}
+              {:name "20", :url "base.url/"}
+              {:name "21", :url "base.url/"}
+              {:name "22", :url "base.url/"}
+              {:name "23", :url "base.url/"}
+              {:name "24", :url "base.url/"}]}
