64e30ee94ac582f333376f4f434916e342e9dcd4
Zefira Shannon <earthlingzephyr@gmail.com>
2015-11-08 23:38:43 -0600

    Correct pages-list test expectation

--- resources/test/manga-reader/image.clj
+++ resources/test/manga-reader/image.clj
@@ -3,27 +3,27 @@
               :src
               "http://i995.mangareader.net/the-gamer/95/the-gamer-5978527.jpg"}]
  :chapter-url "base.url"
- :pages-list [{:name "1", :url "base.url/"}
-              {:name "2", :url "base.url/"}
-              {:name "3", :url "base.url/"}
-              {:name "4", :url "base.url/"}
-              {:name "5", :url "base.url/"}
-              {:name "6", :url "base.url/"}
-              {:name "7", :url "base.url/"}
-              {:name "8", :url "base.url/"}
-              {:name "9", :url "base.url/"}
-              {:name "10", :url "base.url/"}
-              {:name "11", :url "base.url/"}
-              {:name "12", :url "base.url/"}
-              {:name "13", :url "base.url/"}
-              {:name "14", :url "base.url/"}
-              {:name "15", :url "base.url/"}
-              {:name "16", :url "base.url/"}
-              {:name "17", :url "base.url/"}
-              {:name "18", :url "base.url/"}
-              {:name "19", :url "base.url/"}
-              {:name "20", :url "base.url/"}
-              {:name "21", :url "base.url/"}
-              {:name "22", :url "base.url/"}
-              {:name "23", :url "base.url/"}
-              {:name "24", :url "base.url/"}]}
+ :pages-list [{:name "1", :url "base.url/1"}
+              {:name "2", :url "base.url/2"}
+              {:name "3", :url "base.url/3"}
+              {:name "4", :url "base.url/4"}
+              {:name "5", :url "base.url/5"}
+              {:name "6", :url "base.url/6"}
+              {:name "7", :url "base.url/7"}
+              {:name "8", :url "base.url/8"}
+              {:name "9", :url "base.url/9"}
+              {:name "10", :url "base.url/10"}
+              {:name "11", :url "base.url/11"}
+              {:name "12", :url "base.url/12"}
+              {:name "13", :url "base.url/13"}
+              {:name "14", :url "base.url/14"}
+              {:name "15", :url "base.url/15"}
+              {:name "16", :url "base.url/16"}
+              {:name "17", :url "base.url/17"}
+              {:name "18", :url "base.url/18"}
+              {:name "19", :url "base.url/19"}
+              {:name "20", :url "base.url/20"}
+              {:name "21", :url "base.url/21"}
+              {:name "22", :url "base.url/22"}
+              {:name "23", :url "base.url/23"}
+              {:name "24", :url "base.url/24"}]}
