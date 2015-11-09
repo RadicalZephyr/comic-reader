32a77c4ff9b1e0f0a3147aa6b63529450d877b75
Zefira Shannon <earthlingzephyr@gmail.com>
2015-11-09 00:20:36 -0600

    Start implementing manga-reader site

--- resources/sites/manga-reader.clj
+++ resources/sites/manga-reader.clj
@@ -1,1 +1,17 @@
-{}
+{
+ :image-selector [:div#imgholder :a :img#img]
+ :page-normalize-pattern #"(?<=/\d{1,5})/\d+$"
+ :page-normalize-format "%s%s"
+
+ :chapter-number-pattern #"(?<=/\d{1,5})/\d+$"
+
+ :page-list-selector [:div#selectpage :select#pageMenu :option]
+
+ ;; :root-url "http://www.mangareader.net"
+ ;; :manga-url-format "%s/"
+ ;; :manga-url-suffix-pattern #"/(.*?)$"
+
+ ;; :chapter-number-match-pattern #"/(\d+)$"
+
+
+ }
