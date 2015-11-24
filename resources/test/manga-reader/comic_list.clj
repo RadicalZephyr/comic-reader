631f64da5405defa5e088ced2dc8369abb9eb4c1
Zefira Shannon <earthlingzephyr@gmail.com>
2015-11-23 18:02:23 -0800

    Fix manga reader urls to be normalized to absolute urls

--- resources/test/manga-reader/comic_list.clj
+++ resources/test/manga-reader/comic_list.clj
@@ -1,221 +1,223 @@
 {:comic-list
- [{:id "junjou-drop", :name "Junjou Drop", :url "/junjou-drop"}
-  {:id "kanchigai-hime-to-usotsuki-shimobe",
-   :name "Kanchigai Hime to Usotsuki Shimobe",
-   :url "/kanchigai-hime-to-usotsuki-shimobe"}
-  {:id "tobaku-datenroku-kaiji-kazuyahen",
-   :name "Tobaku Datenroku Kaiji: Kazuyahen",
-   :url "/tobaku-datenroku-kaiji-kazuyahen"}
-  {:id "welcome-to-ghost-city",
-   :name "Welcome to Ghost City",
-   :url "/welcome-to-ghost-city"}
-  {:id "000000-ultra-black",
-   :name "#000000 - Ultra Black",
-   :url "/000000-ultra-black"}
-  {:id "anima",
-   :name "+Anima",
-   :url "/anima"}
-  {:id "c-sword-and-cornett",
-   :name "+C Sword and Cornett",
-   :url "/c-sword-and-cornett"}
-  {:id "0-noushiya-minato",
+ [{:id "0-noushiya-minato",
    :name "0-noushiya Minato",
-   :url "/0-noushiya-minato"}
+   :url "http://www.mangareader.net/0-noushiya-minato"}
   {:id "00-mhz",
    :name "0.0 Mhz",
-   :url "/00-mhz"}
+   :url "http://www.mangareader.net/00-mhz"}
+  {:id "000000-ultra-black",
+   :name "#000000 - Ultra Black",
+   :url "http://www.mangareader.net/000000-ultra-black"}
   {:id "009-recyborg",
    :name "009 Re:Cyborg",
-   :url "/009-recyborg"}
+   :url "http://www.mangareader.net/009-recyborg"}
   {:id "07-ghost",
    :name "07 Ghost",
-   :url "/07-ghost"}
+   :url "http://www.mangareader.net/07-ghost"}
   {:id "090-eko-to-issho",
    :name "090 - Eko To Issho",
-   :url "/090-eko-to-issho"}
-  {:id "111",
-   :name "1/11",
-   :url "/111"}
-  {:id "12-love",
-   :name "1/2 Love!",
-   :url "/12-love"}
-  {:id "12-prince",
-   :name "1/2 Prince",
-   :url "/12-prince"}
-  {:id "13-no-kareshi",
-   :name "1/3 No Kareshi",
-   :url "/13-no-kareshi"}
-  {:id "13-romantica",
-   :name "1/3 Romantica",
-   :url "/13-romantica"}
+   :url "http://www.mangareader.net/090-eko-to-issho"}
   {:id "1001",
    :name "1001",
-   :url "/1001"}
+   :url "http://www.mangareader.net/1001"}
   {:id "11-eyes",
    :name "11 Eyes",
-   :url "/11-eyes"}
+   :url "http://www.mangareader.net/11-eyes"}
+  {:id "111",
+   :name "1/11",
+   :url "http://www.mangareader.net/111"}
+  {:id "12-love",
+   :name "1/2 Love!",
+   :url "http://www.mangareader.net/12-love"}
   {:id "12-nin-no-yasashii-koroshiya",
    :name "12 Nin no Yasashii Koroshiya",
-   :url "/12-nin-no-yasashii-koroshiya"}
+   :url "http://www.mangareader.net/12-nin-no-yasashii-koroshiya"}
+  {:id "12-prince",
+   :name "1/2 Prince",
+   :url "http://www.mangareader.net/12-prince"}
   {:id "13-club",
    :name "13 Club",
-   :url "/13-club"}
+   :url "http://www.mangareader.net/13-club"}
+  {:id "13-no-kareshi",
+   :name "1/3 No Kareshi",
+   :url "http://www.mangareader.net/13-no-kareshi"}
+  {:id "13-romantica",
+   :name "1/3 Romantica",
+   :url "http://www.mangareader.net/13-romantica"}
   {:id "14-sai-no-koi",
    :name "14-sai no Koi",
-   :url "/14-sai-no-koi"}
-  {:id "17-years-old-that-summer-days-miracle",
-   :name "17 Years Old, That Summer Day's Miracle",
-   :url "/17-years-old-that-summer-days-miracle"}
+   :url "http://www.mangareader.net/14-sai-no-koi"}
   {:id "17-sai-degree-celsius",
    :name "17-sai Degree Celsius",
-   :url "/17-sai-degree-celsius"}
+   :url "http://www.mangareader.net/17-sai-degree-celsius"}
   {:id "17-sai-kiss-to-dilemma",
    :name "17-sai, Kiss to Dilemma",
-   :url "/17-sai-kiss-to-dilemma"}
+   :url "http://www.mangareader.net/17-sai-kiss-to-dilemma"}
+  {:id "17-years-old-that-summer-days-miracle",
+   :name "17 Years Old, That Summer Day's Miracle",
+   :url "http://www.mangareader.net/17-years-old-that-summer-days-miracle"}
   {:id "18-diary",
    :name "18 Diary",
-   :url "/18-diary"}
+   :url "http://www.mangareader.net/18-diary"}
   {:id "18-years-old-we-got-married",
    :name "18 Years Old, We Got Married",
-   :url "/18-years-old-we-got-married"}
+   :url "http://www.mangareader.net/18-years-old-we-got-married"}
   {:id "2-dome-no-koi-wa-usotsuki",
    :name "2 Dome no Koi wa Usotsuki",
-   :url "/2-dome-no-koi-wa-usotsuki"}
-  {:id "25-jigen-kareshi",
-   :name "2.5 Jigen Kareshi",
-   :url "/25-jigen-kareshi"}
+   :url "http://www.mangareader.net/2-dome-no-koi-wa-usotsuki"}
   {:id "2001-nights",
    :name "2001 Nights",
-   :url "/2001-nights"}
+   :url "http://www.mangareader.net/2001-nights"}
   {:id "20th-century-boys",
    :name "20th Century Boys",
-   :url "/20th-century-boys"}
+   :url "http://www.mangareader.net/20th-century-boys"}
   {:id "21st-century-boys",
    :name "21st Century Boys",
-   :url "/21st-century-boys"}
+   :url "http://www.mangareader.net/21st-century-boys"}
+  {:id "25-jigen-kareshi",
+   :name "2.5 Jigen Kareshi",
+   :url "http://www.mangareader.net/25-jigen-kareshi"}
   {:id "3-am-dangerous-zone",
    :name "3 am Dangerous Zone",
-   :url "/3-am-dangerous-zone"}
+   :url "http://www.mangareader.net/3-am-dangerous-zone"}
   {:id "3-banme-no-kareshi",
    :name "3 Banme no Kareshi",
-   :url "/3-banme-no-kareshi"}
+   :url "http://www.mangareader.net/3-banme-no-kareshi"}
   {:id "3-gatsu-no-lion",
    :name "3 Gatsu no Lion",
-   :url "/3-gatsu-no-lion"}
+   :url "http://www.mangareader.net/3-gatsu-no-lion"}
   {:id "3-street-of-mysteries",
    :name "3, Street of Mysteries",
-   :url "/3-street-of-mysteries"}
+   :url "http://www.mangareader.net/3-street-of-mysteries"}
   {:id "337-byooshi",
    :name "337 Byooshi",
-   :url "/337-byooshi"}
+   :url "http://www.mangareader.net/337-byooshi"}
   {:id "34-sai-mushoku-san",
    :name "34-sai Mushoku-san",
-   :url "/34-sai-mushoku-san"}
+   :url "http://www.mangareader.net/34-sai-mushoku-san"}
   {:id "360-degrees-material",
    :name "360 Degrees Material",
-   :url "/360-degrees-material"}
+   :url "http://www.mangareader.net/360-degrees-material"}
   {:id "37-degrees-kiss",
    :name "37 Degrees Kiss",
-   :url "/37-degrees-kiss"}
+   :url "http://www.mangareader.net/37-degrees-kiss"}
   {:id "3d-kanojo",
    :name "3D Kanojo",
-   :url "/3d-kanojo"}
+   :url "http://www.mangareader.net/3d-kanojo"}
   {:id "3x3-eyes",
    :name "3x3 Eyes",
-   :url "/3x3-eyes"}
+   :url "http://www.mangareader.net/3x3-eyes"}
   {:id "4-cut-hero",
    :name "4 Cut Hero",
-   :url "/4-cut-hero"}
+   :url "http://www.mangareader.net/4-cut-hero"}
   {:id "4-god-ranger",
    :name "4 God Ranger",
-   :url "/4-god-ranger"}
+   :url "http://www.mangareader.net/4-god-ranger"}
   {:id "5-cats",
    :name "5 Cats",
-   :url "/5-cats"}
+   :url "http://www.mangareader.net/5-cats"}
   {:id "5-centimeters-per-second",
    :name "5 Centimeters per Second",
-   :url "/5-centimeters-per-second"}
+   :url "http://www.mangareader.net/5-centimeters-per-second"}
   {:id "5-ji-kara-9-ji-made",
    :name "5-ji kara 9-ji made",
-   :url "/5-ji-kara-9-ji-made"}
+   :url "http://www.mangareader.net/5-ji-kara-9-ji-made"}
   {:id "50-million-km",
    :name "50 Million Km",
-   :url "/50-million-km"}
+   :url "http://www.mangareader.net/50-million-km"}
   {:id "6-no-trigger",
    :name "6 no Trigger",
-   :url "/6-no-trigger"}
+   :url "http://www.mangareader.net/6-no-trigger"}
   {:id "6000",
    :name "6000",
-   :url "/6000"}
+   :url "http://www.mangareader.net/6000"}
   {:id "666-satan",
    :name "666 Satan",
-   :url "/666-satan"}
+   :url "http://www.mangareader.net/666-satan"}
   {:id "7-jikan-me-rhapsody",
    :name "7 Jikan-me Rhapsody",
-   :url "/7-jikan-me-rhapsody"}
-  {:id "7-seeds",
-   :name "7 Seeds",
-   :url "/7-seeds"}
+   :url "http://www.mangareader.net/7-jikan-me-rhapsody"}
   {:id "7-nin-no-shakespeare",
    :name "7-nin no Shakespeare",
-   :url "/7-nin-no-shakespeare"}
+   :url "http://www.mangareader.net/7-nin-no-shakespeare"}
+  {:id "7-seeds",
+   :name "7 Seeds",
+   :url "http://www.mangareader.net/7-seeds"}
   {:id "70",
    :name "70",
-   :url "/70"}
+   :url "http://www.mangareader.net/70"}
   {:id "703x",
    :name "703X",
-   :url "/703x"}
+   :url "http://www.mangareader.net/703x"}
   {:id "7th-dragon",
    :name "7th Dragon",
-   :url "/7th-dragon"}
+   :url "http://www.mangareader.net/7th-dragon"}
   {:id "7th-period-is-a-secret",
    :name "7th Period Is A Secret",
-   :url "/7th-period-is-a-secret"}
-  {:id "81-yamada-yuusuke-gekijou",
-   :name "8.1 - Yamada Yuusuke Gekijou",
-   :url "/81-yamada-yuusuke-gekijou"}
+   :url "http://www.mangareader.net/7th-period-is-a-secret"}
   {:id "81-diver",
    :name "81 Diver",
-   :url "/81-diver"}
+   :url "http://www.mangareader.net/81-diver"}
+  {:id "81-yamada-yuusuke-gekijou",
+   :name "8.1 - Yamada Yuusuke Gekijou",
+   :url "http://www.mangareader.net/81-yamada-yuusuke-gekijou"}
   {:id "87-clockers",
    :name "87 Clockers",
-   :url "/87-clockers"}
+   :url "http://www.mangareader.net/87-clockers"}
   {:id "a-bias-girl",
    :name "A Bias Girl",
-   :url "/a-bias-girl"}
+   :url "http://www.mangareader.net/a-bias-girl"}
   {:id "a-book-of-dreams",
    :name "A Book of Dreams",
-   :url "/a-book-of-dreams"}
+   :url "http://www.mangareader.net/a-book-of-dreams"}
   {:id "a-channel",
    :name "A Channel",
-   :url "/a-channel"}
+   :url "http://www.mangareader.net/a-channel"}
   {:id "a-fairytale-for-the-demon-lord",
    :name "A Fairytale for the Demon Lord",
-   :url "/a-fairytale-for-the-demon-lord"}
+   :url "http://www.mangareader.net/a-fairytale-for-the-demon-lord"}
   {:id "a-falsified-romance",
    :name "A Falsified Romance",
-   :url "/a-falsified-romance"}
+   :url "http://www.mangareader.net/a-falsified-romance"}
   {:id "a-lollipop-or-a-bullet",
    :name "A Lollipop or a Bullet",
-   :url "/a-lollipop-or-a-bullet"}
+   :url "http://www.mangareader.net/a-lollipop-or-a-bullet"}
   {:id "a-love-guard",
    :name "A Love Guard",
-   :url "/a-love-guard"}
+   :url "http://www.mangareader.net/a-love-guard"}
   {:id "a-man-for-megan",
    :name "A Man for Megan",
-   :url "/a-man-for-megan"}
+   :url "http://www.mangareader.net/a-man-for-megan"}
   {:id "a-method-to-make-the-world-gentle",
    :name "A Method to Make the World Gentle",
-   :url "/a-method-to-make-the-world-gentle"}
+   :url "http://www.mangareader.net/a-method-to-make-the-world-gentle"}
   {:id "a-midsummer-nights-dream",
    :name "A Midsummer Night's Dream",
-   :url "/a-midsummer-nights-dream"}
+   :url "http://www.mangareader.net/a-midsummer-nights-dream"}
   {:id "a-rose-colored-tomorrow",
    :name "A Rose-Colored Tomorrow",
-   :url "/a-rose-colored-tomorrow"}
+   :url "http://www.mangareader.net/a-rose-colored-tomorrow"}
   {:id "a-tackle-on-my-life",
    :name "A Tackle on My Life",
-   :url "/a-tackle-on-my-life"}
+   :url "http://www.mangareader.net/a-tackle-on-my-life"}
   {:id "a-thousand-years-ninetails",
    :name "A Thousand Years Ninetails",
-   :url "/a-thousand-years-ninetails"}]}
+   :url "http://www.mangareader.net/a-thousand-years-ninetails"}
+  {:id "anima",
+   :name "+Anima",
+   :url "http://www.mangareader.net/anima"}
+  {:id "c-sword-and-cornett",
+   :name "+C Sword and Cornett",
+   :url "http://www.mangareader.net/c-sword-and-cornett"}
+  {:id "junjou-drop",
+   :name "Junjou Drop",
+   :url "http://www.mangareader.net/junjou-drop"}
+  {:id "kanchigai-hime-to-usotsuki-shimobe",
+   :name "Kanchigai Hime to Usotsuki Shimobe",
+   :url "http://www.mangareader.net/kanchigai-hime-to-usotsuki-shimobe"}
+  {:id "tobaku-datenroku-kaiji-kazuyahen",
+   :name "Tobaku Datenroku Kaiji: Kazuyahen",
+   :url "http://www.mangareader.net/tobaku-datenroku-kaiji-kazuyahen"}
+  {:id "welcome-to-ghost-city",
+   :name "Welcome to Ghost City",
+   :url "http://www.mangareader.net/welcome-to-ghost-city"}]}
