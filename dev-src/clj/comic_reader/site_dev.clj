56ec9a93e53959396f148c0c33ca68da59ab8071
Zefira Shannon <earthlingzephyr@gmail.com>
2015-11-23 17:34:17 -0800

    Rework site-dev to allow running network tests

--- dev-src/clj/comic_reader/site_dev.clj
+++ dev-src/clj/comic_reader/site_dev.clj
@@ -3,13 +3,29 @@
             [clojure.test :refer [run-tests]]
             [clojure.tools.namespace.repl :refer [refresh]]))
 
-(defn run-site-tests* []
+(defn- after-run-site-tests [network?]
   (require 'comic-reader.sites-test)
-  (run-tests (find-ns 'comic-reader.sites-test)))
+  (let [test-ns (find-ns 'comic-reader.sites-test)]
+    (reset! (deref (ns-resolve test-ns 'run-network-tests?)) network?)
+    (run-tests test-ns)))
 
-(defn touch-file [file]
+(defn- *run-site-tests-without-network []
+  (after-run-site-tests false))
+
+(defn- *run-site-tests-with-network []
+  (after-run-site-tests true))
+
+(defn- touch-file [file]
   (.setLastModified (as-file file) (System/currentTimeMillis)))
 
-(defn run-site-tests []
+(defn- *run-site-tests [after-sym]
   (touch-file (resource "comic_reader/sites_test.clj"))
-  (refresh :after 'comic-reader.site-dev/run-site-tests*))
+  (refresh :after after-sym))
+
+(defn run-site-tests []
+  (*run-site-tests
+   'comic-reader.site-dev/*run-site-tests-without-network))
+
+(defn run-site-tests-with-network []
+  (*run-site-tests
+   'comic-reader.site-dev/*run-site-tests-with-network))
