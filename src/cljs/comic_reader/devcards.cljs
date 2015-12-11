be7c22219eff01cb2fb3ff8f9368922fb4e62008
Zefira Shannon <earthlingzephyr@gmail.com>
2015-12-11 16:23:05 -0600

    Get some initial cljs tests running and passing

--- src/cljs/comic_reader/devcards.cljs
+++ src/cljs/comic_reader/devcards.cljs
@@ -1,5 +1,6 @@
 (ns comic-reader.devcards
-  (:require [devcards.core :refer-macros [defcard]]))
+  (:require [devcards.core :refer-macros [defcard]]
+            [comic-reader.main-test]))
 
 (defcard my-first-card
   "# Hello Devcards
