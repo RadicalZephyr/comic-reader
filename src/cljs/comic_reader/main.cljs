03285345895862ff2dec5a02d2b33afbe5eb0b49
Zefira Shannon <earthlingzephyr@gmail.com>
2015-12-11 17:04:53 -0600

    Reorganize code into more sane namespaces

--- src/cljs/comic_reader/main.cljs
+++ src/cljs/comic_reader/main.cljs
@@ -1,13 +1,6 @@
 (ns comic-reader.main
   (:require [reagent.core :as reagent]))
 
-(defn loading []
-  [:img.loading {:src "img/loading.svg"}])
-
-(defn site-list []
-  [:div [:h1 "Comics List"]
-   [loading]])
-
 (defn main-panel
   []
   [:div "Hello Re-Frame!"])
---
+++ test/cljs/comic_reader/ui_test.cljs
@@ -0,0 +1,15 @@
+(ns comic-reader.ui-test
+  (:require [cljs.test :refer-macros [is testing]]
+            [devcards.core :refer-macros [deftest defcard-rg]]
+            [comic-reader.ui :as ui]
+            [comic-reader.main-test]))
+
+(defcard-rg loading-card
+  "# Loading
+   This is the loading svg used everywhere on the site."
+  (ui/loading))
+
+(deftest test-site-list
+  (is (= [:div [:h1 "Comics List"]
+          [ui/loading]]
+         (ui/site-list))))
