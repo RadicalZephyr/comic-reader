93a9016797535fb67ff4884ee5cbcb7f952d4f76
Zefira Shannon <earthlingzephyr@gmail.com>
2015-12-15 11:24:08 -0600

    Move clojurescript macros into a separate ns

--- test/clj/comic_reader/util_test.clj
+++ test/clj/comic_reader/util_test.clj
@@ -1,30 +1,15 @@
 (ns comic-reader.util-test
-  (:require [comic-reader.util :refer :all]
-            [clojure.test :refer :all]))
+  (:require [comic-reader.util :as sut]
+            [clojure.test :refer [deftest is]]))
 
 (deftest keyword->words-test
-  (is (= (keyword->words :manga-fox)
+  (is (= (sut/keyword->words :manga-fox)
          ["manga" "fox"]))
-  (is (= (keyword->words :thingy-doer)
+  (is (= (sut/keyword->words :thingy-doer)
          ["thingy" "doer"])))
 
 (deftest keyword->title-test
-  (is (= (keyword->title :manga-fox)
+  (is (= (sut/keyword->title :manga-fox)
          "Manga Fox"))
-  (is (= (keyword->title :manga-reader)
+  (is (= (sut/keyword->title :manga-reader)
          "Manga Reader")))
-
-(deftest subscription-binding-test
-  (is (thrown? clojure.lang.ExceptionInfo
-               (subscription-binding '(a b))))
-
-  (is (= '[abc (re-frame.core/subscribe [:abc])]
-         (subscription-binding 'abc)))
-  (is (= '[abc (re-frame.core/subscribe [:def])]
-         (subscription-binding '[abc [:def]]))))
-
-(deftest container-name-test
-  (is (= 'abc-container
-         (container-name 'abc)))
-  (is (= 'abc-container
-         (container-name :abc))))
