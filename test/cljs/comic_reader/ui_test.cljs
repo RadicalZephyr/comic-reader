8928cf7adb21b3b0423cc7bffff397407288355d
Geoff Shannon <geoffpshannon@gmail.com>
2015-12-14 22:38:12 -0600

    Implement and test loading image

--- test/cljs/comic_reader/ui_test.cljs
+++ test/cljs/comic_reader/ui_test.cljs
@@ -1,6 +1,7 @@
 (ns comic-reader.ui-test
   (:require [cljs.test :refer-macros [is testing]]
             [devcards.core :refer-macros [deftest defcard-rg]]
+            [re-frame.core :as re-frame]
             [comic-reader.ui :as ui]
             [comic-reader.main-test]))
 
@@ -9,7 +10,13 @@
    This is the loading svg used everywhere on the site."
   (ui/loading))
 
+
 (deftest test-site-list
+  (re-frame/dispatch [:initialize-db])
+  (is (= [:div [:h1 "Comics List"]]
+         ((ui/site-list))))
+
+  (re-frame/dispatch-sync [:fetch-comic-sites])
   (is (= [:div [:h1 "Comics List"]
           [ui/loading]]
-         (ui/site-list))))
+         ((ui/site-list)))))
