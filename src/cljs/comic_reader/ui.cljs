5f4f84df29723d965d5e5f886cf7d5778f8f4add
Zefira Shannon <earthlingzephyr@gmail.com>
2015-12-14 22:38:12 -0600

    Implement and test loading image

--- src/cljs/comic_reader/ui.cljs
+++ src/cljs/comic_reader/ui.cljs
@@ -1,8 +1,29 @@
-(ns comic-reader.ui)
+(ns comic-reader.ui
+  (:require [re-frame.core :as re-frame]
+            [reagent.ratom :refer-macros [reaction]]))
 
 (defn loading []
   [:img.loading {:src "img/loading.svg"}])
 
+(re-frame/register-sub
+ :comic-list-status
+ (fn [db _]
+   (reaction (get-in @db [:comic-list :status]))))
+
+(re-frame/register-handler
+ :fetch-comic-sites
+ (fn [app-state _]
+   (assoc-in app-state [:comic-list :status] :loading)))
+
+(re-frame/register-handler
+ :initialize-db
+ (fn [_ _]
+   {:comic-list {:status :none}}))
+
 (defn site-list []
-  [:div [:h1 "Comics List"]
-   [loading]])
+  (let [status (re-frame/subscribe [:comic-list-status])]
+    (fn []
+      `[:div [:h1 "Comics List"]
+        ~@(case @status
+            :loading [[loading]]
+            nil)])))
