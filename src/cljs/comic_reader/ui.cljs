2fb2b660172f04f0c8fb321eae4612356ca37b4c
Zefira Shannon <earthlingzephyr@gmail.com>
2015-12-14 22:56:19 -0600

    Change site-list component to container/component pattern

--- src/cljs/comic_reader/ui.cljs
+++ src/cljs/comic_reader/ui.cljs
@@ -5,25 +5,8 @@
 (defn loading []
   [:img.loading {:src "img/loading.svg"}])
 
-(re-frame/register-sub
- :comic-list-status
- (fn [db _]
-   (reaction (get-in @db [:comic-list :status]))))
-
-(re-frame/register-handler
- :fetch-comic-sites
- (fn [app-state _]
-   (assoc-in app-state [:comic-list :status] :loading)))
-
-(re-frame/register-handler
- :initialize-db
- (fn [_ _]
-   {:comic-list {:status :none}}))
-
-(defn site-list []
-  (let [status (re-frame/subscribe [:comic-list-status])]
-    (fn []
-      `[:div [:h1 "Comics List"]
-        ~@(case @status
-            :loading [[loading]]
-            nil)])))
+(defn site-list [status]
+  `[:div [:h1 "Comics List"]
+    ~@(case status
+        :loading [[loading]]
+        nil)])
