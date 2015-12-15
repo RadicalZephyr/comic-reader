a4e4f261222eea3e7fe69368e2ad60ba13168d9a
Zefira Shannon <earthlingzephyr@gmail.com>
2015-12-14 23:27:05 -0600

    Implement actual site list and elements

--- test/cljs/comic_reader/ui_test.cljs
+++ test/cljs/comic_reader/ui_test.cljs
@@ -11,10 +11,27 @@
   (ui/loading))
 
 
+(deftest test-site-element
+  (is (= [:li [:a "A"]]
+         (ui/site-element {:name "A"})))
+
+  (is (= {:key "a"}
+         (meta (ui/site-element {:id "a"})))))
+
 (deftest test-site-list
   (is (= [:div [:h1 "Comics List"]]
-         (ui/site-list nil)))
+         (ui/site-list nil nil)))
 
   (is (= [:div [:h1 "Comics List"]
           [ui/loading]]
-         (ui/site-list :loading))))
+         (ui/site-list :loading nil)))
+
+  (is (= [:div [:h1 "Comics List"]
+            [:ul
+             [:li [:a "A"]]
+             [:li [:a "B"]]
+             [:li [:a "C"]]]]
+         (ui/site-list :loaded
+                       [{:name "A" :id "a"}
+                        {:name "B" :id "b"}
+                        {:name "C" :id "c"}]))))
