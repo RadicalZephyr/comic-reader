a7ce7feb0211907ef87a50ab33fc741d271c03ab
Geoff Shannon <geoffpshannon@gmail.com>
2015-12-20 15:48:00 -0600

    Partition ui ns into base and site-list

---
+++ src/cljs/comic_reader/ui/base.cljs
@@ -0,0 +1,18 @@
+(ns comic-reader.ui.base)
+
+(defn loading []
+  [:img.loading {:src "img/loading.svg"}])
+
+(defn four-oh-four []
+  [:div
+   [:h1 "Sorry!"]
+   "There's nothing to see here. Try checking out the "
+   [:a {:href "/#"} "site list."]])
+
+(defn large-button [content]
+  [:a.large.button.radius content])
+
+(defn map-into-list [base-el f coll]
+  (into base-el
+        (map (fn [data]
+               ^{:key (:id data)} [:li (f data)]) coll)))
--- test/cljs/comic_reader/devcards.cljs
+++ test/cljs/comic_reader/devcards.cljs
@@ -1,3 +1,4 @@
 (ns comic-reader.devcards
-  (:require comic-reader.ui-test
+  (:require comic-reader.ui.base-test
+            comic-reader.ui.site-list-test
             comic-reader.main-test))
