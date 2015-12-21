28b405a8a347cb7e07448fce2041a2ec139d6da4
Geoff Shannon <geoffpshannon@gmail.com>
2015-12-20 21:22:39 -0600

    Add tests for map-into-list

--- test/cljs/comic_reader/ui/base_test.cljs
+++ test/cljs/comic_reader/ui/base_test.cljs
@@ -1,5 +1,6 @@
 (ns comic-reader.ui.base-test
-  (:require [devcards.core :refer-macros [defcard-rg]]
+  (:require [cljs.test :refer-macros [is testing]]
+            [devcards.core :refer-macros [deftest defcard-rg]]
             [comic-reader.ui.base :as sut]))
 
 (defcard-rg loading
@@ -13,3 +14,17 @@
 
 (defcard-rg large-button
   [sut/large-button "Button"])
+
+(deftest test-map-into-list
+  (is (= [:ul [:li 1] [:li 2]]
+         (sut/map-into-list [:ul] identity [1 2])))
+
+  (is (= [:ol.everything
+          [:li [:a {:href "a"} "A"]]
+          [:li [:a {:href "b"} "B"]]]
+         (sut/map-into-list [:ol.everything]
+                            (fn [el]
+                              [:a {:href (:url el)}
+                               (:content el)])
+                            [{:url "a" :content "A"}
+                             {:url "b" :content "B"}]))))
