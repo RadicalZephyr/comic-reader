aaba6a489aa0da574dbc5372b8efb74ac7c595f7
Zefira Shannon <earthlingzephyr@gmail.com>
2016-01-15 00:11:09 -0600

    Allow injecting of view-site fn into site-list

--- src/cljs/comic_reader/ui/site_list.cljs
+++ src/cljs/comic_reader/ui/site_list.cljs
@@ -1,8 +1,7 @@
 (ns comic-reader.ui.site-list
   (:require [re-frame.core :as re-frame]
             [reagent.ratom :refer-macros [reaction]]
-            [comic-reader.ui.base :as base])
-  (:require-macros [comic-reader.macro-util :refer [defcomponent-2]]))
+            [comic-reader.ui.base :as base]))
 
 (defn get-site-list [db]
   (get db :site-list))
@@ -20,11 +19,19 @@
   :set-site-list
   set-site-list))
 
-(defcomponent-2 site-list
-  [[sites :site-list]]
+(defn site-list [view-site sites]
   (base/list-with-loading
    {:heading "Comic Sites"
     :list-element [:ul.inline-list]
     :item->li (fn [site]
-                [base/large-button (:name site)])}
+                [:a.large.button.radius
+                 {:on-click #(view-site (:id site))}
+                 (:name site)])}
    sites))
+
+(defn site-list-container []
+  (let [sites (re-frame/subscribe [:site-list])]
+    (fn [] [site-list
+            (fn [site-id]
+              (re-frame/dispatch [:view-site site-id]))
+            (deref sites)])))
