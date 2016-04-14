ffbc1410adbbb2446dfcf7b83c3c31230382480a
Zefira Shannon <earthlingzephyr@gmail.com>
2016-04-13 21:22:49 -0500

    Improve overlay component
    - Make toggle-able
    - Make classes unique per component to allow multiple overlays
      on the same page

--- src/cljs/comic_reader/ui/overlay.cljs
+++ src/cljs/comic_reader/ui/overlay.cljs
@@ -1,14 +1,17 @@
 (ns comic-reader.ui.overlay
-  (:require [garden.core :as g]))
+  (:require [comic-reader.ui.base :as base]
+            [garden.core :as g]))
 
-(defn overlay [{:keys [header footer]} content]
+(defn overlay [{:keys [header footer display? toggle-overlay content]}]
   (let [overlay-styles {:position "absolute"
                         :background "black"
-                        :display "block"
+                        :display (if display? "block" "none")
                         :width "100%"
-                        :padding "0 2em"}]
-    [:div {:style {:position "relative"}}
-     [:style (g/css [:div.overlay overlay-styles [:h2 {:color "white"}]])]
-     [:div.overlay {:style {:top "0"}} header]
-     [:div.overlay {:style {:bottom "0"}} footer]
+                        :padding "0 2em"}
+        overlay-class (base/unique-class :div "overlay")]
+    [:div {:style {:position "relative"}
+           :on-click #(toggle-overlay)}
+     [:style (g/css [overlay-class overlay-styles [:h2 {:color "white"}]])]
+     [overlay-class {:style {:top "0"}} header]
+     [overlay-class {:style {:bottom "0"}} footer]
      content]))
