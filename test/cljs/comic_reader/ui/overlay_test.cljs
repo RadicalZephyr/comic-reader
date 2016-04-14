a14a7c04a50b2802fbbda8a1f73e1be5ba777aa6
Geoff Shannon <geoffpshannon@gmail.com>
2016-04-13 21:34:54 -0500

    DRY up overlay tests

--- test/cljs/comic_reader/ui/overlay_test.cljs
+++ test/cljs/comic_reader/ui/overlay_test.cljs
@@ -5,6 +5,13 @@
             [comic-reader.ui.base :as base])
   (:require-macros [comic-reader.macro-util :refer [reactively]]))
 
+(def dummy-content
+  [:div
+   [:h2 "Content Title"]
+   [:p "Body paragraph"]
+   [:p "Body paragraph"]
+   [:p "Body paragraph"]])
+
 (defcard-rg overlay-on
   (fn [data _]
     (let [toggle-overlay (fn []
@@ -16,12 +23,7 @@
          :footer [:h2 "Bottom"]
          :display? (:overlay @data)
          :toggle-overlay toggle-overlay
-         :content
-         [:div
-          [:h2 "Content Title"]
-          [:p "Body paragraph"]
-          [:p "Body paragraph"]
-          [:p "Body paragraph"]]}])))
+         :content dummy-content}])))
   (reagent/atom {:overlay true})
   {:inspect-data true})
 
@@ -36,11 +38,6 @@
          :footer [:h2 "Bottom"]
          :display? (:overlay @data)
          :toggle-overlay toggle-overlay
-         :content
-         [:div
-          [:h2 "Content Title"]
-          [:p "Body paragraph"]
-          [:p "Body paragraph"]
-          [:p "Body paragraph"]]}])))
+         :content dummy-content}])))
   (reagent/atom {:overlay false})
   {:inspect-data true})
