e71ce9fab5f238d7daf8846978dac5943544e141
Geoff Shannon <geoffpshannon@gmail.com>
2016-04-13 21:22:49 -0500

    Improve overlay component
    - Make toggle-able
    - Make classes unique per component to allow multiple overlays
      on the same page

--- test/cljs/comic_reader/ui/overlay_test.cljs
+++ test/cljs/comic_reader/ui/overlay_test.cljs
@@ -1,19 +1,46 @@
 (ns comic-reader.ui.overlay-test
   (:require [devcards.core :refer-macros [defcard-rg]]
             [reagent.core :as reagent]
-            [comic-reader.ui.overlay :as sut]))
+            [comic-reader.ui.overlay :as sut]
+            [comic-reader.ui.base :as base])
+  (:require-macros [comic-reader.macro-util :refer [reactively]]))
 
 (defcard-rg overlay-on
-  [:div
-   [sut/overlay {:header [:h2 "Top"] :footer [:h2 "Bottom"]}
-    [:div
-     [:h2 "Content Title"]
-     [:p "Body paragraph"]
-     [:p "Body paragraph"]
-     [:p "Body paragraph"]]]
-   [sut/overlay {:header [:h2 "Top"] :footer [:h2 "Bottom"]}
-    [:div
-     [:h2 "Content Title"]
-     [:p "Body paragraph"]
-     [:p "Body paragraph"]
-     [:p "Body paragraph"]]]])
+  (fn [data _]
+    (let [toggle-overlay (fn []
+                           (base/do-later
+                            #(swap! data update :overlay not)))]
+      (reactively
+       [sut/overlay
+        {:header [:h2 "Top"]
+         :footer [:h2 "Bottom"]
+         :display? (:overlay @data)
+         :toggle-overlay toggle-overlay
+         :content
+         [:div
+          [:h2 "Content Title"]
+          [:p "Body paragraph"]
+          [:p "Body paragraph"]
+          [:p "Body paragraph"]]}])))
+  (reagent/atom {:overlay true})
+  {:inspect-data true})
+
+(defcard-rg overlay-off
+  (fn [data _]
+    (let [toggle-overlay (fn []
+                           (base/do-later
+                            #(swap! data update :overlay not)))]
+      (reactively
+       [sut/overlay
+        {:header [:h2 "Top"]
+         :footer [:h2 "Bottom"]
+         :display? (:overlay @data)
+         :toggle-overlay toggle-overlay
+         :content
+         [:div
+          [:h2 "Content Title"]
+          [:p "Body paragraph"]
+          [:p "Body paragraph"]
+          [:p "Body paragraph"]]}])))
+  (reagent/atom {:overlay false})
+  {:inspect-data true})
