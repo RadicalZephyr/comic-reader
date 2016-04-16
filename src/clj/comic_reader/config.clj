4fc74b03864651e9c6916fe83f768ed896e7c661
Zefira Shannon <earthlingzephyr@gmail.com>
2016-04-15 18:50:56 -0700

    Create a config protocol with methods for values

--- src/clj/comic_reader/config.clj
+++ src/clj/comic_reader/config.clj
@@ -2,6 +2,10 @@
   (:require [com.stuartsierra.component :as component]
             [environ.core :refer [env]]))
 
+(defprotocol PConfig
+  (database-uri [cfg])
+  (norms-dir [cfg]))
+
 (defrecord Config [database-uri norms-dir]
   component/Lifecycle
 
@@ -11,7 +15,18 @@
            :database-uri (env :database-uri)
            :norms-dir    (env :norms-dir)))
 
-  (stop [component]))
+  (stop [component])
+
+  PConfig
+  (database-uri [cfg] (:database-uri cfg))
+
+  (norms-dir [cfg] (:norms-dir cfg)))
+
+(extend-type clojure.lang.APersistentMap
+  PConfig
+  (database-uri [cfg] (:database-uri cfg))
+
+  (norms-dir [cfg] (:norms-dir cfg)))
 
 (defn new-config []
   (map->Config {}))
--- src/clj/comic_reader/database.clj
+++ src/clj/comic_reader/database.clj
@@ -1,27 +1,25 @@
 (ns comic-reader.database
   (:require [com.stuartsierra.component :as component]
+            [comic-reader.config :as config]
             [comic-reader.database.norms :as norms]
             [datomic.api :as d]
-            [io.rkn.conformity :as c]))
-
-(defn- database-uri [config]
-  (:database-uri config))
+            [io.rkn.conformity :as conformity]))
 
 (defn- create-database [config]
-  (d/create-database (database-uri config)))
+  (d/create-database (config/database-uri config)))
 
 (defn- connect [config]
-  (d/connect (database-uri config)))
+  (d/connect (config/database-uri config)))
 
 (defn- setup-and-connect-to-database [config]
-  (when (database-uri config)
+  (when (config/database-uri config)
     (println "Comic-Reader: Connecting to database...")
     (let [do-seeds? (create-database config)
           conn (connect config)]
       ;; Conform database to schemas here
-      (when-let [norms-dir (:norms-dir config)]
+      (when-let [norms-dir (config/norms-dir config)]
         (println "Comic-Reader: Conforming database to norms...")
-        (c/ensure-conforms conn (norms/norms-map norms-dir)))
+        (conformity/ensure-conforms conn (norms/norms-map norms-dir)))
 
       ;; Add seeds if database was newly created
       ;; (when do-seeds?
