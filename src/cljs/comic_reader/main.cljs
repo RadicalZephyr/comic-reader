e9eb55d8fc47103011edcc951233813868079f47
Zefira Shannon <earthlingzephyr@gmail.com>
2016-04-16 00:09:13 -0700

    Add a very basic stubbed main page

--- src/cljs/comic_reader/main.cljs
+++ src/cljs/comic_reader/main.cljs
@@ -1,13 +1,22 @@
 (ns comic-reader.main
-  (:require [reagent.core :as reagent]))
+  (:require
+    [comic-reader.ui.base :as base]
+    [comic-reader.ui.site-list :as site-list]
+    [reagent.core :as reagent]
+    [re-frame.core :as re-frame]))
 
 (defn main-panel
-  []
-  [:div "Hello Re-Frame!"])
+  [page-key]
+  (case page-key
+    :site [site-list/site-list-container]
+    [base/four-oh-four]))
 
 (defn ^:export main
   []
-  (reagent/render-component [main-panel]
+  (site-list/setup!)
+
+  (re-frame/dispatch [:set-site-list [{:id :a :name "Comic A"}]])
+  (reagent/render-component [main-panel :site]
                             (.getElementById js/document "app")))
 
 (main)
