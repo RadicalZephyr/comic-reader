6c3b74d282b84043511fe832c6fbc0985d69495b
Geoff Shannon <geoffpshannon@gmail.com>
2016-04-15 20:32:39 -0700

    Clean up norms and norms-test nses

--- test/clj/comic_reader/database/norms_test.clj
+++ test/clj/comic_reader/database/norms_test.clj
@@ -6,21 +6,21 @@
 (defn- file-resource [name]
   (-> name io/resource io/as-file))
 
-(deftest test-norms-seq
+(deftest norms-seq-test
 
   (is (nil? (sut/norms-seq "database/no-norms")))
 
-  (is (= (set (map #(io/as-file (io/resource %))
+  (is (= (set (map file-resource
                    ["database/test-norms/this-fake-norm.edn"
                     "database/test-norms/enumeration-norm.edn"]))
          (set (sut/norms-seq "database/test-norms")))))
 
-(deftest test-files->norms-map
+(deftest files->norms-map-test
 
   (is (= {:this-fake-norm {:txes [[]]}}
          (sut/files->norms-map [(file-resource "database/test-norms/this-fake-norm.edn")]))))
 
-(deftest test-norms-map
+(deftest norms-map-test
   (is nil? (sut/norms-map "database/no-norms"))
 
   (is (= {:this-fake-norm {:txes [[]]}}
