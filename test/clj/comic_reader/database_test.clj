773c3171a262966501e0dfcaecc0b42420ae4f22
Zefira Shannon <earthlingzephyr@gmail.com>
2016-04-15 19:36:14 -0700

    Add test showing norms being conformed

--- test/clj/comic_reader/database_test.clj
+++ test/clj/comic_reader/database_test.clj
@@ -1,7 +1,8 @@
 (ns comic-reader.database-test
   (:require [clojure.test :refer :all]
             [com.stuartsierra.component :as component]
-            [comic-reader.database :as sut]))
+            [comic-reader.database :as sut]
+            [datomic.api :as d]))
 
 (deftest test-database-component
   (let [db (sut/new-database)]
@@ -12,10 +13,22 @@
 
      (is (nil? (:conn not-started-db))))
 
-    (let [config {:database-uri "datomic:mem://comics"
+    (let [config {:database-uri "datomic:mem://comics-test"
                   :norms-dir nil}
           db (assoc db :config config)]
       (is (not (nil? (sut/get-conn db))))
 
       (let [started-db (component/start db)]
-        (is (not (nil? (:conn started-db))))))))
+        (is (not (nil? (:conn started-db))))))
+
+    (let [config {:database-uri "datomic:mem://comics-test"
+                  :norms-dir "database/test-norms"}
+          db (-> db
+                 (assoc :config config)
+                 component/start)]
+      (is (= 1
+             (count
+              (d/q '[:find ?e
+                     :in $
+                     :where [?e :db/ident :test.enum/one]]
+                   (d/db (:conn db)))))))))
