76e2abb95dbb60d370172f2073b98df163b75635
Zefira Shannon <earthlingzephyr@gmail.com>
2016-05-15 20:31:33 -0500

    Add a namespace for accumulating dynamic css

--- test/cljs/comic_reader/devcards.cljs
+++ test/cljs/comic_reader/devcards.cljs
@@ -2,7 +2,8 @@
   (:require comic-reader.main-test
             comic-reader.api-test
             comic-reader.ui.base-test
-            comic-reader.ui.site-list-test
             comic-reader.ui.comic-list-test
+            comic-reader.ui.component-css-test
             comic-reader.ui.image-test
-            comic-reader.ui.overlay-test))
+            comic-reader.ui.overlay-test
+            comic-reader.ui.site-list-test))
