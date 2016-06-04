03ee14f7f09029cd17460c2961cf5ed6f3a89630
Geoff Shannon <geoffpshannon@gmail.com>
2016-06-04 18:58:49 -0500

    Start writing a scraper repo
    This will implement the actual fetching logic of chaining together a
    sequence of calls to the scraper.

---
+++ test/clj/comic_reader/comic_repository/scraper_test.clj
@@ -0,0 +1,20 @@
+(ns comic-reader.comic-repository.scraper-test
+  (:require [clojure.test :as t]
+            [comic-reader.comic-repository.scraper :as sut]
+            [comic-reader.mock-site-scraper :refer [mock-scraper]]
+            [com.stuartsierra.component :as component]))
+
+(defn scraper-test-system [scraper]
+  (component/system-map
+   :site-scraper scraper
+   :scraper-repo (component/using
+                  (sut/new-scraper-repo)
+                  [:site-scraper])))
+
+(defn test-repo [scraper]
+  (-> (scraper-test-system scraper)
+      (component/start)
+      :scraper-repo))
+
+(t/deftest test-creation
+  (t/is (not (nil? (test-repo (mock-scraper :sites []))))))
