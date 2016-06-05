7fed04b01508c794958a80cbcc67c7c46f9f839f
Geoff Shannon <geoffpshannon@gmail.com>
2016-06-05 13:48:51 -0500

    Use a lazy-seq to cross chapter boundaries

--- src/clj/comic_reader/comic_repository/scraper.clj
+++ src/clj/comic_reader/comic_repository/scraper.clj
@@ -2,14 +2,18 @@
   (:require [comic-reader.comic-repository.protocol :as protocol]
             [comic-reader.site-scraper :as site-scraper]))
 
+(defn page-seq [scraper site chapters]
+  (lazy-seq
+   (concat (site-scraper/list-pages scraper site (first chapters))
+           (page-seq scraper site (rest chapters)))))
+
 (defrecord ScraperRepository [scraper]
   protocol/ComicRepository
   (previous-pages [this site comic-id location n])
+
   (next-pages [this site comic-id location n]
-    (let [chapters (site-scraper/list-chapters scraper site comic-id)
-          chapter (first chapters)
-          pages (site-scraper/list-pages scraper site chapter)]
-      (cond->> pages
+    (let [chapters (site-scraper/list-chapters scraper site comic-id)]
+      (cond->> (page-seq scraper site chapters)
         location (drop-while #(not= % location))
         :always (take n)))))
 
--- test/clj/comic_reader/comic_repository/scraper_test.clj
+++ test/clj/comic_reader/comic_repository/scraper_test.clj
@@ -47,4 +47,23 @@
           location {:name "2", :url  "url2"}]
       (t/is (= [{:name "2", :url  "url2"}
                 {:name "3", :url  "url3"}]
-               (repo-protocol/next-pages repo "manga-fox" "the-gamer" location 2))))))
+               (repo-protocol/next-pages repo "manga-fox" "the-gamer" location 2)))))
+
+  (t/testing "it crosses chapter boundaries to fetch n pages"
+    (let [repo (test-repo (mock-scraper :chapters {"manga-fox"
+                                                   {"the-gamer" [{:name "The Gamer 1" :ch-num 1}
+                                                                 {:name "The Gamer 2" :ch-num 2}]}}
+                                        :pages {"manga-fox"
+                                                {{:name "The Gamer 1" :ch-num 1}
+                                                 [{:name "1", :url  "url1"}
+                                                  {:name "2", :url  "url2"}
+                                                  {:name "3", :url  "url3"}]
+
+                                                 {:name "The Gamer 2" :ch-num 2}
+                                                 [{:name "4", :url  "url4"}
+                                                  {:name "5", :url  "url5"}]}}))
+          location {:name "3", :url  "url3"}]
+      (t/is (= [{:name "3", :url  "url3"}
+                {:name "4", :url  "url4"}
+                {:name "5", :url  "url5"}]
+               (repo-protocol/next-pages repo "manga-fox" "the-gamer" location 3))))))
