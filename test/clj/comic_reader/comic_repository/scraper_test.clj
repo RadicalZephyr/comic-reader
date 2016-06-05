b62771f22a65cb105ad91a52ce49635899925f82
Geoff Shannon <geoffpshannon@gmail.com>
2016-06-04 19:39:10 -0500

    Add a basic implementation of comic-walking
    It currently always starts at the beginning and doesn't care whether it
    returns n pages if that would cause it to cross a chapter boundary.

--- test/clj/comic_reader/comic_repository/scraper_test.clj
+++ test/clj/comic_reader/comic_repository/scraper_test.clj
@@ -1,5 +1,6 @@
 (ns comic-reader.comic-repository.scraper-test
   (:require [clojure.test :as t]
+            [comic-reader.comic-repository.protocol :as repo-protocol]
             [comic-reader.comic-repository.scraper :as sut]
             [comic-reader.mock-site-scraper :refer [mock-scraper]]
             [com.stuartsierra.component :as component]))
@@ -9,7 +10,7 @@
    :site-scraper scraper
    :scraper-repo (component/using
                   (sut/new-scraper-repo)
-                  [:site-scraper])))
+                  {:scraper :site-scraper})))
 
 (defn test-repo [scraper]
   (-> (scraper-test-system scraper)
@@ -17,4 +18,19 @@
       :scraper-repo))
 
 (t/deftest test-creation
-  (t/is (not (nil? (test-repo (mock-scraper))))))
+  (let [repo (test-repo (mock-scraper))]
+    (t/is (not (nil? repo)))
+    (t/is (not (nil? (:scraper repo))))))
+
+(t/deftest test-next-pages
+  (t/testing "starts at the beginning when location is nil"
+    (let [repo (test-repo (mock-scraper :chapters {"manga-fox"
+                                                   {"the-gamer" [{:name "The Gamer 1",
+                                                                  :ch-num 1}]}}
+                                        :pages {"manga-fox"
+                                                {{:name "The Gamer 1" :ch-num 1}
+                                                 [{:name "1", :url  "http://www.mangahere.co/manga/the_gamer/c002/"}
+                                                  {:name "2", :url  "http://www.mangahere.co/manga/the_gamer/c002/2.html"}]}}))]
+
+      (t/is (= [{:name "1", :url  "http://www.mangahere.co/manga/the_gamer/c002/"}]
+               (repo-protocol/next-pages repo "manga-fox" "the-gamer" {} 1))))))
