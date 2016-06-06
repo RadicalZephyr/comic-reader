0eed727c3d33eed824d6e248bd17f80d9d442330
Zefira Shannon <earthlingzephyr@gmail.com>
2016-06-05 20:04:39 -0500

    Move comic-repository protocol into root ns

--- src/clj/comic_reader/comic_repository/protocol.clj
+++
@@ -1,8 +0,0 @@
-(ns comic-reader.comic-repository.protocol)
-
-(defprotocol ComicRepository
-  (list-sites         [this] "List all the comic sites available from this repository.")
-  (list-comics        [this site] "List all the comics available on this site.")
-  (previous-locations [this site comic-id location n] "Get up-to n locations that precede `location' in a comic.")
-  (next-locations     [this site comic-id location n] "Get up-to n locations that follow `location' in a comic.")
-  (image-tag          [this site location] "Get the hiccup image tag for this comic location."))
