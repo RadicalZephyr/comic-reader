96df5bb8c28846ec1270598d49ae6494c56db049
Geoff Shannon <geoffpshannon@gmail.com>
2016-06-06 21:22:51 -0500

    Switch to using http-kit for the server

--- src/clj/comic_reader/server.clj
+++ src/clj/comic_reader/server.clj
@@ -1,36 +1,28 @@
 (ns comic-reader.server
   (:require [comic-reader.web-app :as web-app]
             [com.stuartsierra.component :as component]
-            [ring.adapter.jetty :refer [run-jetty]])
-  (:import org.eclipse.jetty.server.Server))
+            [org.httpkit.server :as server]))
 
-(defrecord WebServer [port ^Server server web-app]
+(defrecord WebServer [port stop-server web-app]
   component/Lifecycle
 
   (start [component]
-    (if server
+    (if stop-server
       component
       (do
-        (printf "Comic-Reader: Starting web server on port: %d ...\n"
-                port)
+        (printf "Comic-Reader: Starting web server on port: %d ...\n" port)
         (assoc component
-               :server (run-jetty (web-app/get-routes web-app)
-                                  {:port port
-                                   :join? false})))))
+               :stop-server (server/run-server (web-app/get-routes web-app)
+                                               {:port port})))))
 
   (stop [component]
-    (if-not server
+    (if-not stop-server
       component
       (do
-        (when-not (or (.isStopped server)
-                      (.isStopping server))
-          (println "Comic-Reader: Shutting down web server...")
-          (.stop server))
-        (if (.isStopped server)
-          (assoc component
-                 :web-app nil
-                 :server nil)
-          component)))))
+        (println "Comic-Reader: Shutting down web server...")
+        (stop-server)
+        (assoc component
+               :stop-server nil)))))
 
 (defn new-server [port]
   (map->WebServer {:port port}))
