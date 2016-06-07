e86d0c80ef127992b84dca05df639474e04f3b74
Zefira Shannon <earthlingzephyr@gmail.com>
2016-06-06 21:36:37 -0500

    Split out env config into it's own ns

--- src/clj/comic_reader/config.clj
+++ src/clj/comic_reader/config.clj
@@ -1,33 +1,11 @@
-(ns comic-reader.config
-  (:require [com.stuartsierra.component :as component]
-            [environ.core :refer [env]]))
+(ns comic-reader.config)
 
 (defprotocol Config
   (database-uri [cfg])
   (norms-dir [cfg]))
-
-(defrecord EnvConfig [database-uri norms-dir]
-  component/Lifecycle
-
-  (start [component]
-    (println "Comic-Reader: Loading configuration...")
-    (assoc component
-           :database-uri (env :database-uri)
-           :norms-dir    (env :norms-dir)))
-
-  (stop [component]
-    component)
-
-  Config
-  (database-uri [cfg] (:database-uri cfg))
-
-  (norms-dir [cfg] (:norms-dir cfg)))
 
 (extend-type clojure.lang.APersistentMap
   Config
   (database-uri [cfg] (:database-uri cfg))
 
   (norms-dir [cfg] (:norms-dir cfg)))
-
-(defn new-config []
-  (map->EnvConfig {}))
