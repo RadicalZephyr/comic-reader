972ddae709169de91c14474399527670764aa8c6
Zefira Shannon <earthlingzephyr@gmail.com>
2016-06-06 21:22:51 -0500

    Switch to using http-kit for the server

--- project.clj
+++ project.clj
@@ -34,8 +34,9 @@
 
                  ;; Web server
                  [ring "1.4.0"]
-                 [fogus/ring-edn "0.3.0"]
+                 [http-kit "2.1.19"]
                  [compojure "1.5.0"]
+                 [fogus/ring-edn "0.3.0"]
                  [hiccup "1.0.5"]
                  [garden "1.3.2"]
 
