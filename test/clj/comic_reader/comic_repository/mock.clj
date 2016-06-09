623bd772c53e0211a8e5da360af929dee719bb95
Geoff Shannon <geoffpshannon@gmail.com>
2016-06-08 23:38:03 -0500

    Setup testing infrastructure for datomic repository

---
+++ test/clj/comic_reader/comic_repository/mock.clj
@@ -0,0 +1,23 @@
+(ns comic-reader.comic-repository.mock
+  (:require [comic-reader.comic-repository :as repo]))
+
+(extend-type clojure.lang.IPersistentMap
+  repo/ComicRepository
+  (list-sites [this]
+    (:sites this))
+
+  (list-comics [this site]
+    (get-in this [:comics site]))
+
+  (previous-locations [this site comic-id location n]
+    (take n (get-in this [site :comics comic-id location :previous-locations])))
+
+  (next-locations [this site comic-id location n]
+    (take n (get-in this [site :comics comic-id location :next-locations])))
+
+  (image-tag [this site location]
+    (get-in site location)))
+
+(defn mock-repo
+  ([] {})
+  ([& {:as mock-data}] mock-data))
