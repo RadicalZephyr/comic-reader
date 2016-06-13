ef7f866d4652849ee7c858a52a35b48bdccf328e
Geoff Shannon <geoffpshannon@gmail.com>
2016-06-12 19:23:42 -0500

    Move datomic data access into datomic repository
    Why does this break other random shit?

--- src/clj/comic_reader/comic_repository.clj
+++ src/clj/comic_reader/comic_repository.clj
@@ -6,3 +6,8 @@
   (previous-locations [this site comic-id location n] "Get up-to n locations that precede `location' in a comic.")
   (next-locations     [this site comic-id location n] "Get up-to n locations that follow `location' in a comic.")
   (image-tag          [this site location] "Get the hiccup image tag for this comic location."))
+
+(defprotocol WritableComicRepository
+  (store-sites [this sites] "Store a seq of site records.")
+  (store-comics [this site-id comics] "Store a seq of comic records.")
+  (store-locations [this site-id comic-id locations] "Store a seq of location records."))
--- test/clj/comic_reader/comic_repository/datomic_test.clj
+++ test/clj/comic_reader/comic_repository/datomic_test.clj
@@ -1,38 +1,171 @@
 (ns comic-reader.comic-repository.datomic-test
-  (:require [clojure.core.async :as async :refer [<!!]]
-            [clojure.test :as t]
+  (:require [clojure.test :refer :all]
             [comic-reader.comic-repository :as repo]
             [comic-reader.comic-repository.datomic :as sut]
-            [comic-reader.comic-repository.mock :refer [mock-repo]]
             [comic-reader.database :as database]
-            [com.stuartsierra.component :as component]))
+            [com.stuartsierra.component :as component]
+            [datomic.api :as d]))
 
-(defn datomic-test-system [source-repo]
+(defn datomic-test-system []
   (component/system-map
-   :config {:database-uri "datomic:mem://comics-test"}
+   :config {:database-uri "datomic:mem://comics-test"
+            :norms-dir "database/norms"}
    :database (component/using
               (database/new-database)
               [:config])
-   :source-repo source-repo
    :datomic-repo (component/using
                   (sut/new-repository)
-                  [:database :source-repo])))
+                  [:database])))
 
-(defn test-repo [source-repo]
-  (-> (datomic-test-system source-repo)
-      (component/start)
-      :datomic-repo))
+(defmacro with-test-db [db-binding & body]
+  `(let [test-system# (component/start (datomic-test-system))
+         ~db-binding (:datomic-repo test-system#)]
+     (try
+       ~@body
+       (finally
+         (d/delete-database (get-in test-system# [:config :database-uri]))))))
 
-(t/deftest test-list-sites
-  (t/testing "passes directly through to source-repo"
-    (t/is (= [] (<!! (repo/list-sites (test-repo (mock-repo :sites (async/to-chan [[]])))))))
+(deftest test-get-and-store-sites
+  (testing "returns no results for an empty database"
+    (with-test-db test-database
+      (is (= [] (repo/list-sites test-database)))))
 
-    (let [sites [{:id "site-one", :name "Site One"}
-                 {:id "site-two", :name "Site Two"}
-                 {:id "site-three", :name "Site Three"}]]
-      (t/is (= sites (<!! (repo/list-sites (test-repo (mock-repo :sites (async/to-chan [sites]))))))))))
+  (testing "returns one stored site record"
+    (with-test-db test-database
+      (repo/store-sites test-database [{:id "site-one", :name "Site One"}])
+      (is (= [{:site/id "site-one" :site/name "Site One"}]
+             (repo/list-sites test-database)))))
 
-(t/deftest test-list-comics
-  (t/testing "returns a core.async channel"
-    (let [repo (test-repo (mock-repo :comics {"manga-fox" (async/to-chan [[]])}))]
-      (t/is (= [] (<!! (repo/list-comics repo "manga-fox")))))))
+  (testing "returns many stored site records"
+    (with-test-db test-database
+      (repo/store-sites test-database [{:id "site-one", :name "Site One"}
+                                      {:id "site-two", :name "Site Two"}])
+      (is (= [{:site/id "site-one" :site/name "Site One"}
+              {:site/id "site-two" :site/name "Site Two"}]
+             (repo/list-sites test-database))))))
+
+(deftest test-get-and-store-comics
+  (testing "returns no results for an empty database"
+    (with-test-db test-database
+      (is (= [] (repo/list-comics test-database "site-one")))))
+
+  (testing "returns one stored comic record"
+    (with-test-db test-database
+      (let [site {:id "site-one", :name "Site One"}]
+        @(repo/store-sites test-database [site])
+        @(repo/store-comics test-database (:id site) [{:id "comic-one" :name "Comic One"}])
+
+        (is (= [{:comic/id "comic-one" :comic/name "Comic One"}]
+               (repo/list-comics test-database "site-one"))))))
+
+  (testing "returns one stored comic record"
+    (with-test-db test-database
+      (let [site {:id "site-one", :name "Site One"}
+            other-site {:id "site-two", :name "Site Two"}]
+        @(repo/store-sites test-database [site other-site])
+        @(repo/store-comics test-database (:id site) [{:id "comic-one" :name "Comic One"}])
+        @(repo/store-comics test-database (:id other-site) [{:id "not-good-comic" :name "Not Good Comic"}])
+
+        (is (= [{:comic/id "comic-one" :comic/name "Comic One"}]
+               (repo/list-comics test-database "site-one"))))))
+
+  (testing "returns many stored comic records"
+    (with-test-db test-database
+      (let [site {:id "site-one", :name "Site One"}]
+        @(repo/store-sites test-database [site])
+        @(repo/store-comics test-database (:id site) [{:id "comic-one" :name "Comic One"}
+                                                     {:id "comic-two" :name "Comic Two"}]))
+
+      (is (= #{{:comic/id "comic-one" :comic/name "Comic One"}
+               {:comic/id "comic-two" :comic/name "Comic Two"}}
+             (set (repo/list-comics test-database "site-one")))))))
+
+(deftest test-get-and-store-locations
+  (testing "returns no results for an empty database"
+    (with-test-db test-database
+      (is (= [] (repo/next-locations test-database "site-one" "comic-one" nil 100)))))
+
+  (testing "returns one location"
+    (with-test-db test-database
+      (let [site {:id "site-one", :name "Site One"}
+            comic {:id "comic-one" :name "Comic One"}]
+        @(repo/store-sites test-database [site])
+        @(repo/store-comics test-database (:id site) [comic])
+        @(repo/store-locations test-database (:id site) (:id comic)
+                              [{:chapter {:name "The Gamer 1" :ch-num 1} :page {:number 1 :url  "url1"}}])
+
+        (is (= [{:location/chapter {:chapter/title "The Gamer 1" :chapter/number 1}
+                 :location/page {:page/number 1 :page/url  "url1"}}]
+               (repo/next-locations test-database "site-one" "comic-one" nil 100))))))
+
+  (testing "returns multiple locations"
+    (with-test-db test-database
+      (let [site {:id "site-one", :name "Site One"}
+            comic {:id "comic-one" :name "Comic One"}]
+        @(repo/store-sites test-database [site])
+        @(repo/store-comics test-database (:id site) [comic])
+        @(repo/store-locations test-database (:id site) (:id comic)
+                              [{:chapter {:name "The Gamer 1" :ch-num 1} :page {:number 1 :url  "url1"}}
+                               {:chapter {:name "The Gamer 2" :ch-num 2} :page {:number 2 :url  "url2"}}])
+
+        (is (= [{:location/chapter {:chapter/title "The Gamer 1" :chapter/number 1}
+                 :location/page {:page/number 1 :page/url  "url1"}}
+                {:location/chapter {:chapter/title "The Gamer 2" :chapter/number 2}
+                 :location/page {:page/number 2 :page/url  "url2"}}]
+               (repo/next-locations test-database "site-one" "comic-one" nil 100))))))
+
+  (testing "only returns location for the desired site and comic"
+    (with-test-db test-database
+      (let [site {:id "site-one", :name "Site One"}
+            other-site {:id "site-two" :name "Site Two"}
+            comic {:id "comic-one" :name "Comic One"}
+            other-comic {:id "comic-other" :name "Comic One"}
+            third-comic {:id "comic-third" :name "Comic Third"}]
+
+        @(repo/store-sites test-database [site other-site])
+        @(repo/store-comics test-database (:id site) [comic third-comic])
+        @(repo/store-locations test-database (:id site) (:id comic)
+                              [{:chapter {:name "The Gamer 1" :ch-num 1} :page {:number 1 :url  "url1"}}
+                               {:chapter {:name "The Gamer 2" :ch-num 2} :page {:number 2 :url  "url2"}}])
+        @(repo/store-locations test-database (:id site) (:id third-comic)
+                              [{:chapter {:name "The Third 1" :ch-num 1} :page {:number 1 :url  "third-url1"}}
+                               {:chapter {:name "The Third 2" :ch-num 2} :page {:number 2 :url  "third-url2"}}])
+
+
+        @(repo/store-comics test-database (:id other-site) [other-comic])
+        @(repo/store-locations test-database (:id other-site) (:id other-comic)
+                              [{:chapter {:name "The Other 1" :ch-num 1} :page {:number 1 :url  "other-url1"}}
+                               {:chapter {:name "The Other 2" :ch-num 2} :page {:number 2 :url  "other-url2"}}])
+
+        (is (= [{:location/chapter {:chapter/title "The Gamer 1" :chapter/number 1}
+                 :location/page {:page/number 1 :page/url  "url1"}}
+                {:location/chapter {:chapter/title "The Gamer 2" :chapter/number 2}
+                 :location/page {:page/number 2 :page/url  "url2"}}]
+               (repo/next-locations test-database "site-one" "comic-one" nil 100))))))
+
+  (testing "Sorts all locations by chapter and page number"
+    (with-test-db test-database
+      (let [site {:id "site-one", :name "Site One"}
+            comic {:id "comic-one" :name "Comic One"}]
+        @(repo/store-sites test-database [site])
+        @(repo/store-comics test-database (:id site) [comic])
+        @(repo/store-locations test-database (:id site) (:id comic)
+                              [{:chapter {:name "The Gamer 1" :ch-num 1} :page {:number 1 :url  "url1"}}
+                               {:chapter {:name "The Gamer 1" :ch-num 1} :page {:number 2 :url  "url2"}}
+                               {:chapter {:name "The Gamer 1" :ch-num 1} :page {:number 3 :url  "url3"}}
+
+                               {:chapter {:name "The Gamer 2" :ch-num 2} :page {:number 1 :url  "url1"}}
+                               {:chapter {:name "The Gamer 2" :ch-num 2} :page {:number 2 :url  "url2"}}])
+
+        (is (= [{:location/chapter {:chapter/title "The Gamer 1" :chapter/number 1}
+                 :location/page {:page/number 1 :page/url  "url1"}}
+                {:location/chapter {:chapter/title "The Gamer 1" :chapter/number 1}
+                 :location/page {:page/number 2 :page/url  "url2"}}
+                {:location/chapter {:chapter/title "The Gamer 1" :chapter/number 1}
+                 :location/page {:page/number 3 :page/url  "url3"}}
+
+                {:location/chapter {:chapter/title "The Gamer 2" :chapter/number 2}
+                 :location/page {:page/number 1 :page/url  "url1"}}
+                {:location/chapter {:chapter/title "The Gamer 2" :chapter/number 2}
+                 :location/page {:page/number 2 :page/url  "url2"}}]
+               (repo/next-locations test-database "site-one" "comic-one" nil 100)))))))
