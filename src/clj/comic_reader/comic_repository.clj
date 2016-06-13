5c0c1a331f8d1e565dbf47fcc3e171059eaae65a
Geoff Shannon <geoffpshannon@gmail.com>
2016-06-12 19:33:03 -0500

    Use site-id in ComicRepository protocol method arguments

--- src/clj/comic_reader/comic_repository.clj
+++ src/clj/comic_reader/comic_repository.clj
@@ -2,10 +2,10 @@
 
 (defprotocol ComicRepository
   (list-sites         [this] "List all the comic sites available from this repository.")
-  (list-comics        [this site] "List all the comics available on this site.")
-  (previous-locations [this site comic-id location n] "Get up-to n locations that precede `location' in a comic.")
-  (next-locations     [this site comic-id location n] "Get up-to n locations that follow `location' in a comic.")
-  (image-tag          [this site location] "Get the hiccup image tag for this comic location."))
+  (list-comics        [this site-id] "List all the comics available on this site.")
+  (previous-locations [this site-id comic-id location n] "Get up-to n locations that precede `location' in a comic.")
+  (next-locations     [this site-id comic-id location n] "Get up-to n locations that follow `location' in a comic.")
+  (image-tag          [this site-id location] "Get the hiccup image tag for this comic location."))
 
 (defprotocol WritableComicRepository
   (store-sites [this sites] "Store a seq of site records.")
