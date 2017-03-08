3e5bac34c81350480a8fb729df7f8532efabb112
Geoff Shannon <geoffpshannon@gmail.com>
2017-03-08 09:52:44 -0800

    Fix manga fox test data file

--- resources/test/manga-fox/image.clj
+++ resources/test/manga-fox/image.clj
@@ -3,26 +3,26 @@
               "http://a.mfcdn.net/store/manga/13088/02-095.0/compressed/m001.jpg"
               :alt "The Gamer 95 at MangaFox.me"}]
 
- :pages-list [{:page/number 1, ::page/url "/1.html"}
-              {:page/number 2, ::page/url "/2.html"}
-              {:page/number 3, ::page/url "/3.html"}
-              {:page/number 4, ::page/url "/4.html"}
-              {:page/number 5, ::page/url "/5.html"}
-              {:page/number 6, ::page/url "/6.html"}
-              {:page/number 7, ::page/url "/7.html"}
-              {:page/number 8, ::page/url "/8.html"}
-              {:page/number 9, ::page/url "/9.html"}
-              {:page/number 10, ::page/url "/10.html"}
-              {:page/number 11, ::page/url "/11.html"}
-              {:page/number 12, ::page/url "/12.html"}
-              {:page/number 13, ::page/url "/13.html"}
-              {:page/number 14, ::page/url "/14.html"}
-              {:page/number 15, ::page/url "/15.html"}
-              {:page/number 16, ::page/url "/16.html"}
-              {:page/number 17, ::page/url "/17.html"}
-              {:page/number 18, ::page/url "/18.html"}
-              {:page/number 19, ::page/url "/19.html"}
-              {:page/number 20, ::page/url "/20.html"}
-              {:page/number 21, ::page/url "/21.html"}
-              {:page/number 22, ::page/url "/22.html"}
-              {:page/number 23, ::page/url "/23.html"}]}
+ :pages-list [{:page/number 1, :page/url "/1.html"}
+              {:page/number 2, :page/url "/2.html"}
+              {:page/number 3, :page/url "/3.html"}
+              {:page/number 4, :page/url "/4.html"}
+              {:page/number 5, :page/url "/5.html"}
+              {:page/number 6, :page/url "/6.html"}
+              {:page/number 7, :page/url "/7.html"}
+              {:page/number 8, :page/url "/8.html"}
+              {:page/number 9, :page/url "/9.html"}
+              {:page/number 10, :page/url "/10.html"}
+              {:page/number 11, :page/url "/11.html"}
+              {:page/number 12, :page/url "/12.html"}
+              {:page/number 13, :page/url "/13.html"}
+              {:page/number 14, :page/url "/14.html"}
+              {:page/number 15, :page/url "/15.html"}
+              {:page/number 16, :page/url "/16.html"}
+              {:page/number 17, :page/url "/17.html"}
+              {:page/number 18, :page/url "/18.html"}
+              {:page/number 19, :page/url "/19.html"}
+              {:page/number 20, :page/url "/20.html"}
+              {:page/number 21, :page/url "/21.html"}
+              {:page/number 22, :page/url "/22.html"}
+              {:page/number 23, :page/url "/23.html"}]}
