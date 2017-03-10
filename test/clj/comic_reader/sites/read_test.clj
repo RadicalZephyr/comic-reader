c498b327134673ac4dcdb0d7694e694b867ab1ab
Geoff Shannon <geoffpshannon@gmail.com>
2017-03-09 21:55:56 -0800

    Fix sites.read-test
    These tests were modifying the classpath file system and dependent on
    the idea that they could write into the classpath at will by putting
    things under "resources".
    
    The price of boot classpath isolation is more complicated testing
    setup machinery to mess with the test classpath. Upside is the tests
    are much more robustly implemented because of the logic extraction.

--- test/clj/comic_reader/sites/read_test.clj
+++ test/clj/comic_reader/sites/read_test.clj
@@ -1,4 +1,5 @@
 (ns comic-reader.sites.read-test
+  (:refer-clojure :rename {spit core-spit})
   (:require [clojure.java.io :as io]
             [clojure.test :as t]
             [comic-reader.sites.read :as sut]))
@@ -15,33 +16,67 @@
   (t/is (= (test-base-name "dir/two/three/four.clj")
            "four")))
 
-(t/deftest find-all-sites-test
-  (let [test-file (io/as-file "resources/sites/test-site.clj")]
-    (spit test-file "{}")
-    (t/is (some #{"test-site"} (sut/find-all-sites)))
-    (io/delete-file test-file))
+(defn- get-file-mapping [[resource-name content]]
+  (let [file (io/as-file (io/resource resource-name))
+        old-content (slurp file)]
+    {:file file
+     :old-content old-content
+     :content content}))
 
-  (let [test-file (io/as-file "resources/sites/abc")]
-    (spit test-file "nothing important")
-    (t/is (= (not-any? #{"abc"} (sut/find-all-sites))))
-    (io/delete-file test-file)))
+(defn- spit [f content]
+  (if (nil? content)
+    (io/delete-file f)
+    (core-spit f content)))
+
+(defn with-file-contents-fn [file-mappings body-fn]
+  (let [file-mappings (map get-file-mapping (partition 2 file-mappings))]
+    (doseq [{:keys [file content]} file-mappings]
+      (spit file content))
+    (body-fn)
+    (doseq [{:keys [file old-content]} file-mappings]
+      (spit file old-content))))
+
+(defmacro with-file-contents [file-mappings & body]
+  `(with-file-contents-fn ~file-mappings
+     (fn []
+       ~@body)))
+
+(t/deftest find-all-sites-test
+  (with-file-contents ["sites/test-site.clj" "{}"
+                       "sites/sites-list.clj" nil]
+    (t/is (= true
+             (contains? (set (sut/find-all-sites)) "test-site"))))
+
+  (with-file-contents ["sites/test-site.clj"  nil
+                       "sites/sites-list.clj" nil]
+    (t/is (= false
+             (contains? (set (sut/find-all-sites)) "test-site"))))
+
+  (with-file-contents ["sites/test-site.clj"  ""
+                       "sites/sites-list.clj" nil]
+    (t/is (= true
+             (contains? (set (sut/find-all-sites)) "test-site"))))
+
+  (with-file-contents ["sites/test-site.clj"  "nonsense-site-definition"
+                       "sites/sites-list.clj" nil]
+    (t/is (= true
+             (contains? (set (sut/find-all-sites)) "test-site")))))
 
 (t/deftest read-site-options-test
   (t/is (= nil (sut/read-site-options "non-existent")))
 
-  (let [test-file (io/as-file "resources/sites/test-site.clj")]
-    (spit test-file "{}")
+  (with-file-contents ["sites/test-site.clj" "{}"
+                       "sites/sites-list.clj" nil]
     (t/is (= (class (sut/read-site-options "test-site"))
-             clojure.lang.PersistentArrayMap))
-    (io/delete-file test-file)))
+             clojure.lang.PersistentArrayMap))))
 
 (t/deftest get-sites-list-test
+  (with-file-contents ["sites/test-site.clj" "{}"
+                       "sites/sites-list.clj" nil]
+    (with-redefs [sut/find-all-sites (constantly ["a" "b" "c"])]
+      (t/is (= ["a" "b" "c"] (sut/get-sites-list)))))
 
-  (with-redefs [sut/find-all-sites (constantly ["a" "b" "c"])]
-    (t/is (= ["a" "b" "c"] (sut/get-sites-list))))
-
-  (let [test-file (io/file "resources" sut/sites-list-file-name)]
-    (spit test-file  (prn-str ["abc"]))
-    (t/is (= ["abc"]
-             (sut/get-sites-list)))
-    (io/delete-file test-file :silently)))
+  (with-file-contents ["sites/test-site.clj" "{}"
+                       "sites/sites-list.clj" "test-site"]
+    (t/is (= true
+             (contains? (set (sut/get-sites-list)) "test-site")))))
