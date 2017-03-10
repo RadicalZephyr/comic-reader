ed37293563169997d152b2c80e57515e1ab31241
Zefira Shannon <earthlingzephyr@gmail.com>
2017-03-09 23:01:19 -0800

    Make datomic database tests more robust about cleaning up

--- test/clj/comic_reader/database_test.clj
+++ test/clj/comic_reader/database_test.clj
@@ -2,7 +2,25 @@
   (:require [clojure.test :refer :all]
             [com.stuartsierra.component :as component]
             [comic-reader.database :as sut]
+            [comic-reader.database.test-util :as test-util]
             [datomic.api :as d]))
+
+(defn test-system [norms-dir]
+  (component/system-map
+   :config {:database-uri "datomic:mem://comics-test"
+            :norms-dir norms-dir}
+   :database (component/using
+              (sut/new-database)
+              [:config])
+   :db-cleaner (component/using
+                (test-util/new-database-cleaner)
+                [:database])))
+
+(defn wrap-cleanup-datomic [f]
+  (f)
+  (d/shutdown false))
+
+(use-fixtures :each wrap-cleanup-datomic)
 
 (deftest test-database-component
   (let [db (sut/new-database)]
@@ -13,25 +31,27 @@
     (testing "with no config, no connection is started"
       (is (nil? (:conn (component/start db)))))
 
-    (let [config {:database-uri "datomic:mem://comics-test"
-                  :norms-dir nil}
-          db (assoc db :config config)]
+    (let [system (test-system nil)]
       (testing "connection is nil before start"
-        (is (nil? (:conn db))))
+        (is (nil? (:conn (:database system)))))
 
       (testing "connection is non-nil after successful start"
-        (let [started-db (component/start db)]
-          (is (not (nil? (:conn started-db)))))))
+
+        (let [started-system (component/start system)]
+          (try
+            (is (not (nil? (:conn (:database started-system)))))
+            (finally
+              (component/stop started-system))))))
 
     (testing "norms conformation happens on startup"
-      (let [config {:database-uri "datomic:mem://comics-test"
-                    :norms-dir "database/test-norms"}
-            db (-> db
-                   (assoc :config config)
-                   component/start)]
-        (is (= 1
-               (count
-                (d/q '[:find ?e
-                       :in $
-                       :where [?e :db/ident :test.enum/one]]
-                     (d/db (:conn db))))))))))
+      (let [system (component/start (test-system "database/test-norms"))
+            db (:database system)]
+        (try
+          (is (= 1
+                 (count
+                  (d/q '[:find ?e
+                         :in $
+                         :where [?e :db/ident :test.enum/one]]
+                       (d/db (:conn db))))))
+          (finally
+            (component/stop system)))))))
