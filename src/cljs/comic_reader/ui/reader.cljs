1cb5f25693f19fe84a68a4af51310deb26b5eb04
Zefira Shannon <earthlingzephyr@gmail.com>
2017-03-11 23:01:12 -0800

    Make a devcard that demonstrates the waypoints working

--- src/cljs/comic_reader/ui/reader.cljs
+++ src/cljs/comic_reader/ui/reader.cljs
@@ -1,5 +1,13 @@
-(ns comic-reader.ui.reader)
+(ns comic-reader.ui.reader
+  (:require [comic-reader.ui.image :as image]))
 
-(defn comic-reader [images current-location]
-  (into [:div]
-        (map :image/tag images)))
+(defn- image-id [image]
+  (:image/location image))
+
+(defn- make-comic-image [set-current-location image]
+  (with-meta
+    [image/comic-image #(set-current-location (:image/location image)) (:image/tag image)]
+    {:key (image-id image)}))
+
+(defn comic-image-list [set-current-location images current-location]
+  [:div (map #(make-comic-image set-current-location %) images)])
