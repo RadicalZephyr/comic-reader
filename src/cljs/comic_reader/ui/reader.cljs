aaa3ba90e71c39c45c86137a753821b5bcd30359
Geoff Shannon <geoffpshannon@gmail.com>
2017-03-12 01:07:42 -0800

    Start structuring the data needed for the reader view

--- src/cljs/comic_reader/ui/reader.cljs
+++ src/cljs/comic_reader/ui/reader.cljs
@@ -1,5 +1,68 @@
 (ns comic-reader.ui.reader
-  (:require [comic-reader.ui.image :as image]))
+  (:require [re-frame.core :as re-frame]
+            [comic-reader.ui.image :as image]))
+
+(defn partitioned-images [images current-location]
+  (if (seq images)
+    (->> (concat [nil] images [nil])
+         (partition-by #(= (:image/location %) current-location))
+         (map #(keep identity %)))
+    []))
+
+(defn current-images [partitioned-images n]
+  (let [[before current after] partitioned-images]
+    (concat (take-last n before) current (take n after))))
+
+(defn setup! []
+  (re-frame/reg-sub
+   :images
+   (fn [app-db _]
+     (:images app-db)))
+
+  (re-frame.core/reg-event-db
+   :set-images
+   (fn [app-db [_ images]]
+     (assoc app-db :images images)))
+
+  (re-frame/reg-sub
+   (fn [app-db _]
+     (:current-location app-db)))
+
+  (re-frame/reg-event-db
+   :set-current-location
+   (fn [app-db [_ current-location]]
+     (assoc app-db :current-location current-location)))
+
+  (re-frame/reg-sub
+   :partitioned-images
+   :<- [:images]
+   :<- [:current-location]
+   (fn [[images current-location] _]
+     (partitioned-images images current-location)))
+
+  (re-frame/reg-sub
+   :preceding-images-count
+   :<- [:partitioned-images]
+   (fn [[preceding _ _] _]
+     (count preceding)))
+
+  (re-frame/reg-sub
+   :following-images-count
+   :<- [:partitioned-images]
+   (fn [[_ _ following] _]
+     (count following)))
+
+  (re-frame/reg-sub
+   :current-images
+   :<- [:partitioned-images]
+   :<- [:preceding-images-count]
+   :<- [:following-images-count]
+   (fn [[partitioned-images preceding-count following-count] [_ n]]
+     (when (> (/ n 2) preceding-count)
+       )
+     (when (> (/ n 2) following-count)
+       )
+     (current-images partitioned-images n))))
 
 (defn- image-id [image]
   (:image/location image))
@@ -9,5 +72,8 @@
     [image/comic-image #(set-current-location (:image/location image)) (:image/tag image)]
     {:key (image-id image)}))
 
-(defn comic-image-list [set-current-location images current-location]
+(defn comic-image-list [set-current-location images]
   [:div (map #(make-comic-image set-current-location %) images)])
+
+(defn view []
+  )
--- test/cljs/comic_reader/ui/reader_test.cljs
+++ test/cljs/comic_reader/ui/reader_test.cljs
@@ -8,6 +8,65 @@
             [comic-reader.ui.reader :as sut]
             [comic-reader.macro-util :refer-macros [reactively]]))
 
+(deftest test-partitioned-images
+  (testing "handles no data gracefully"
+    (is (= []
+           (sut/partitioned-images [] nil)))
+
+    (is (= []
+           (sut/partitioned-images nil nil)))
+
+    (is (= []
+           (sut/partitioned-images nil :location/abc))))
+
+  (testing "partitions based on the given image location"
+    (is (= [[1] [{:image/location :a}] [2]]
+           (sut/partitioned-images [1 {:image/location :a} 2] :a)))
+
+    (is (= [[1] [{:image/location :a}] [2 3 4]]
+           (sut/partitioned-images [1 {:image/location :a} 2 3 4] :a)))
+
+    (is (= [[-1 0 1] [{:image/location :a}] [2]]
+           (sut/partitioned-images [-1 0 1 {:image/location :a} 2] :a))))
+
+  (testing "returns meaningful partitions when there are no preceding items"
+    (is (= [[] [{:image/location :a}] [2]]
+           (sut/partitioned-images [{:image/location :a} 2] :a))))
+
+  (testing "returns meaningful partitions when there are no following items"
+    (is (= [[1 2] [{:image/location :a}] []]
+           (sut/partitioned-images [1 2 {:image/location :a}] :a)))))
+
+(deftest test-current-images
+  (testing "can handle having no images"
+    (is (= []
+           (sut/current-images [] 1))))
+
+  (testing "can handle not having any previous items"
+    (is (= [:current 2]
+           (sut/current-images [[] [:current] [2 3]]  1))))
+
+  (testing "can handle not having any following items"
+    (is (= [2 :current]
+           (sut/current-images [[1 2] [:current] []] 1))))
+
+  (testing "can select 1 on either side of the current location"
+    (is (= [1 {:image/location :a} 2]
+           (sut/current-images [[1] [{:image/location :a}] [2]] 1)))
+
+    (is (= [2 {:image/location :a} 3]
+           (sut/current-images [[1 2] [{:image/location :a}] [3 4]]   1))))
+
+  (testing "selects n on either side of the current location"
+    (is (= [1 2 {:image/location :a} 3 4]
+           (sut/current-images [[1 2] [{:image/location :a}] [3 4]] 2))))
+
+  (testing "Only selects as many images as have been loaded"
+    (is (= [1 {:image/location :a} 2]
+           (sut/current-images [[1] [{:image/location :a}] [2]] 10)))
+    (is (= [1 {:image/location :a} 2 3 4]
+           (sut/current-images [[1] [{:image/location :a}] [2 3 4]] 10)))))
+
 (defcard-rg comic-image-list
   (fn [data _]
     (let [set-current #(swap! data assoc :current-location %)
@@ -27,7 +86,7 @@
                        {:position "fixed"
                         :top "10px"
                         :left "10px"}]]]])}}]
-        [sut/comic-image-list set-current images :abc]])))
+        [sut/comic-image-list set-current images]])))
   (reagent/atom {})
   {:inspect-data true})
 
