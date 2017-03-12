5074f88a90a988390436ead05f15441f3f345350
Geoff Shannon <geoffpshannon@gmail.com>
2017-03-11 16:31:33 -0800

    Get all devcards working again properly

--- test/cljs/comic_reader/main_test.cljs
+++ test/cljs/comic_reader/main_test.cljs
@@ -1,4 +1,9 @@
 (ns comic-reader.main-test
   (:require [cljs.test :refer-macros [is testing]]
             [devcards.core :refer-macros [deftest]]
-            [comic-reader.main :as main]))
+            [comic-reader.main :as main]
+            [re-frame.core :as re-frame]))
+
+(defn reload []
+  (re-frame/clear-subscription-cache!)
+  (main/main))
