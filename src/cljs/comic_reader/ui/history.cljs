97b67808d02772a127b42e236a432ea4164d923b
Zefira Shannon <earthlingzephyr@gmail.com>
2017-03-27 23:22:45 -0500

    Add a history namespace for wrapping goog.History

---
+++ src/cljs/comic_reader/ui/history.cljs
@@ -0,0 +1,22 @@
+(ns comic-reader.ui.history
+  (:refer-clojure :exclude [get])
+  (:require [goog.events :as events]
+            [goog.history.EventType :as EventType])
+  (:import (goog History)))
+
+(defonce h (History. false))
+
+(defn add-listener! [listener]
+  (events/listen h EventType/NAVIGATE #(listener (.-token %))))
+
+(defn setup! []
+  (.setEnabled h true))
+
+(defn get []
+  (.getToken h))
+
+(defn set! [token & [title]]
+  (.setToken h token (or title "")))
+
+(defn replace! [token & [title]]
+  (.replaceToken h token (or title "")))
