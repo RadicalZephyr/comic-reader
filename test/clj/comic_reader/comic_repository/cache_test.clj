b53638a3f38b3ad32a94a37cd9aecbeb628493cf
Geoff Shannon <geoffpshannon@gmail.com>
2017-08-21 01:31:31 -0600

    Refer in the spy-repo constructor

--- test/clj/comic_reader/comic_repository/cache_test.clj
+++ test/clj/comic_reader/comic_repository/cache_test.clj
@@ -3,7 +3,7 @@
             [comic-reader.comic-repository :as repo]
             [comic-reader.comic-repository.cache :as sut]
             [comic-reader.comic-repository.mock :refer [mock-repo]]
-            [comic-reader.comic-repository.spy :as spy]
+            [comic-reader.comic-repository.spy :as spy :refer [spy-repo]]
             [com.stuartsierra.component :as component]))
 
 (defn cache-test-system [source-repo storage-repo]
@@ -23,7 +23,7 @@
   (testing "fetches data from the source-repo and stores it in the storage-repo"
     (testing "for list-sites"
       (let [sites [{:site/id "site-one" :site/name "Site One"}]
-            spy-repo (spy/spy-repo)
+            spy-repo (spy-repo)
             mock-repo (mock-repo :sites sites)
             test-repo (test-repo mock-repo spy-repo)]
         (is (= sites (repo/list-sites test-repo)))
@@ -31,7 +31,7 @@
 
     (testing "for list-comics"
       (let [comics [{:comic/id "comic-one" :comic/name "Comic One"}]
-            spy-repo (spy/spy-repo)
+            spy-repo (spy-repo)
             mock-repo (mock-repo :comics {:site-one comics})
             test-repo (test-repo mock-repo spy-repo)]
         (is (= comics (repo/list-comics test-repo :site-one)))
@@ -40,7 +40,7 @@
     (testing "for previous-locations"
       (let [location [:fake-comic-location]
             previous-locations [:loc-1 :loc-2]
-            spy-repo (spy/spy-repo)
+            spy-repo (spy-repo)
             mock-repo (mock-repo :site-one {:comics {"comic-one" {location {:previous-locations previous-locations}}}})
             test-repo (test-repo mock-repo spy-repo)]
         (is (= previous-locations
@@ -51,7 +51,7 @@
     (testing "for next-locations"
       (let [location [:fake-comic-location]
             next-locations [:loc-1 :loc-2]
-            spy-repo (spy/spy-repo)
+            spy-repo (spy-repo)
             mock-repo (mock-repo :site-one {:comics {"comic-one" {location {:next-locations next-locations}}}})
             test-repo (test-repo mock-repo spy-repo)]
         (is (= next-locations
@@ -62,7 +62,7 @@
     (testing "for image-tag"
       (let [image-tag [:img {:src "img.jpg"}]
             location [:fake-comic-location]
-            spy-repo (spy/spy-repo)
+            spy-repo (spy-repo)
             mock-repo (mock-repo :site-one {location image-tag})
             test-repo (test-repo mock-repo spy-repo)]
         (is (= image-tag (repo/image-tag test-repo :site-one location)))
