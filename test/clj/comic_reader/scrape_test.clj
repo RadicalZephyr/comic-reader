786c28eca097873dca8cd6c4d773b7df6c36a4f2
Geoff Shannon <geoffpshannon@gmail.com>
2017-08-20 18:18:53 -0600

    Remove unused fetch-* functions
    All they do is compose fetch-url and extract functions, and that
    composition got moved into the sites ns at some point.

--- test/clj/comic_reader/scrape_test.clj
+++ test/clj/comic_reader/scrape_test.clj
@@ -33,13 +33,6 @@
     (t/is (= (sut/extract-image-tag html [:div :p :img])
              [:img {:src "img-two" :alt "text-alt"}]))))
 
-(t/deftest fetch-image-tag-test
-  (let  [html (html [:div {} [:img {:src "dummy-img"
-                                    :alt "alt-text"}]])]
-    (with-redefs [sut/fetch-url (fn [url] html)]
-      (t/is (= (sut/fetch-image-tag {:url "abc" :selector [:div :img]})
-               [:img {:src "dummy-img" :alt "alt-text"}])))))
-
 (t/deftest extract-list-test
   (let [html (html [:div {} [:p {}]])]
     (t/is (= (sut/extract-list html [:div :p] :tag)
@@ -48,11 +41,3 @@
              [{}]))
     (t/is (= (sut/extract-list html [:div :p] (comp seq :content))
              []))))
-
-(t/deftest fetch-list-test
-  (let [html (html [:div {} [:p {}]])]
-    (with-redefs [sut/fetch-url (fn [url] html)]
-      (t/is (= (sut/fetch-list {:url "abc"
-                                :selector [:div :p]
-                                :normalize :tag})
-               [:p])))))
