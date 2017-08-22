b5b54cdb25922472e50f1909b8e451fd0f3f68ad
Zefira Shannon <earthlingzephyr@gmail.com>
2017-08-22 17:27:28 -0600

    Start over with cache repo

--- src/clj/comic_reader/comic_repository/cache.clj
+++ src/clj/comic_reader/comic_repository/cache.clj
@@ -6,33 +6,24 @@
   repo/ComicRepository
   (-list-sites [this]
     (async/go
-      (let [sites (<! (repo/list-sites source-repo))]
-        (repo/store-sites storage-repo sites)
-        sites)))
+      (let [stored-sites (<! (repo/list-sites storage-repo))]
+        (if (seq stored-sites)
+          stored-sites
+          (let [fetched-sites (<! (repo/list-sites source-repo))]
+            (repo/store-sites storage-repo fetched-sites)
+            fetched-sites)))))
 
   (-list-comics [this site-id]
-    (async/go
-      (let [comics (<! (repo/list-comics source-repo site-id))]
-        (repo/store-comics storage-repo comics)
-        comics)))
+    )
 
   (-previous-locations [this comic-id location n]
-    (async/go
-      (let [locations (<! (repo/previous-locations source-repo comic-id location n))]
-        (repo/store-locations storage-repo comic-id locations)
-        locations)))
+    )
 
   (-next-locations [this comic-id location n]
-    (async/go
-      (let [locations (<! (repo/next-locations source-repo comic-id location n))]
-        (repo/store-locations storage-repo comic-id locations)
-        locations)))
+    )
 
   (-image-tag [this site-id location]
-    (async/go
-      (let [image-tag (<! (repo/image-tag source-repo site-id location))]
-
-        image-tag))))
+    ))
 
 (defn new-caching-repository []
   (map->CachingRepository {}))
--- test/clj/comic_reader/comic_repository/cache_test.clj
+++ test/clj/comic_reader/comic_repository/cache_test.clj
@@ -15,52 +15,32 @@
                 (sut/new-caching-repository)
                 [:storage-repo :source-repo])))
 
-(defn test-repo [storage-repo & mock-data]
-  (let [source-repo (apply mock-repo mock-data)]
+(defn mock-spy-system [& mock-data]
+  (let [source-repo (apply mock-repo mock-data)
+        storage-repo (spy-repo)]
     (-> (cache-test-system source-repo storage-repo)
         component/start
-        :cache-repo)))
+        ((juxt :storage-repo :cache-repo)))))
 
-(deftest test-fetch-and-store
-  (testing "fetches data from the source-repo and stores it in the storage-repo"
-    (testing "for list-sites"
-      (let [sites [{:site/id :site-one :site/name "Site One"}]
-            spy-repo (spy-repo)
-            test-repo (test-repo spy-repo :sites sites)]
-        (is (= sites (<!! (repo/list-sites test-repo))))
-        (is (= [{:args [sites]}] (spy/calls spy-repo :store-sites)))))
+(defn spy-mock-system [& mock-data]
+  (let [source-repo (spy-repo)
+        storage-repo (apply mock-repo mock-data)]
+    (-> (cache-test-system source-repo storage-repo)
+        component/start
+        ((juxt :source-repo :cache-repo)))))
 
-    (testing "for list-comics"
-      (let [comics [{:comic/id :site-one/comic-one :comic/name "Comic One"}]
-            spy-repo (spy-repo)
-            test-repo (test-repo spy-repo :comics {:site-one comics})]
-        (is (= comics (<!! (repo/list-comics test-repo :site-one))))
-        (is (= [{:args [comics]}] (spy/calls spy-repo :store-comics)))))
+(deftest test-list-sites
+  (testing "checks storage repo for content first"
+    (let [sites [{:site/id :site-one :site/name "Site One"}]
+          [spy-repo test-repo] (spy-mock-system :sites sites)]
+      (is (= sites (<!! (repo/list-sites test-repo))))
+      (is (= 0 (count (spy/calls spy-repo :list-sites))))))
 
-    (testing "for previous-locations"
-      (let [location [:fake-comic-location]
-            previous-locations [:loc-1 :loc-2]
-            spy-repo (spy-repo)
-            test-repo (test-repo spy-repo :site-one {:comics {"comic-one" {location {:previous-locations previous-locations}}}})]
-        (is (= previous-locations
-               (<!! (repo/previous-locations test-repo :site-one/comic-one location 2))))
-        (is (= [{:args [:site-one/comic-one previous-locations]}]
-               (spy/calls spy-repo :store-locations)))))
+  (testing "when storage repo returns nothing, fetches from source"
+    (let [sites [{:site/id :site-one :site/name "Site One"}]
+          [spy-repo test-repo] (mock-spy-system :sites sites)]
+      (is (= sites (<!! (repo/list-sites test-repo))))
+      (is (= 1 (count (spy/calls spy-repo :list-sites))))
 
-    (testing "for next-locations"
-      (let [location [:fake-comic-location]
-            next-locations [:loc-1 :loc-2]
-            spy-repo (spy-repo)
-            test-repo (test-repo spy-repo :site-one {:comics {"comic-one" {location {:next-locations next-locations}}}})]
-        (is (= next-locations
-               (<!! (repo/next-locations test-repo :site-one/comic-one location 2))))
-        (is (= [{:args [:site-one/comic-one next-locations]}]
-               (spy/calls spy-repo :store-locations)))))
-
-    (testing "for image-tag"
-      (let [image-tag [:img {:src "img.jpg"}]
-            location [:fake-comic-location]
-            spy-repo (spy-repo)
-            test-repo (test-repo spy-repo :site-one {location image-tag})]
-        (is (= image-tag (<!! (repo/image-tag test-repo :site-one location))))
-        #_(is (= [{:args [:site-one location]}] (spy/calls spy-repo :store-image-tag)))))))
+      (testing "and stores the data from source into storage"
+        (is (= [{:args [sites]}] (spy/calls spy-repo :store-sites)))))))
