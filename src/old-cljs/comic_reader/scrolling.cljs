421d1e381f703d851489bcf4b99034e2213256f9
Zefira Shannon <earthlingzephyr@gmail.com>
2017-08-22 20:23:36 -0600

    Remove old cljs and unused cljs history ns

--- src/old-cljs/comic_reader/scrolling.cljs
+++
@@ -1,6 +0,0 @@
-(ns comic-reader.scrolling
-  (:require [comic-reader.events :as e]
-            [re-frame.core :as rf]))
-
-(defn setup-scrolling-events! []
-  (e/throttle "scroll" #(rf/dispatch [:scroll]) 1000))
